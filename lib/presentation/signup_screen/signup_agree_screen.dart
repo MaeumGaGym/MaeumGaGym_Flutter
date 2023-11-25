113116675b67f87e37b7d80e6573cc90d3bf2946
ljyo2o9 <yggosxoljy@gmail.com>
2023-11-25 14:37:04 +0900

    🗒️ :: [#13] signupAgreeScreen

---
+++ lib/presentation/signup_screen/signup_agree_screen.dart
@@ -0,0 +1,234 @@
+/// Package
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:styled_widget/styled_widget.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+
+/// Core
+import 'package:wewith_flutter/core/component/text/pretendard/ptd_text_widget.dart';
+import 'package:wewith_flutter/core/component/check_botton.dart';
+import 'package:wewith_flutter/core/constant/maeumgagym_color.dart';
+
+/// widget
+import 'package:wewith_flutter/presentation/signup_screen/widget/check_box_widget.dart';
+import 'package:wewith_flutter/presentation/signup_screen/provider/provider/check_box_widget_provider.dart';
+
+class SignUpAgreeScreen extends StatelessWidget {
+  const SignUpAgreeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var click = Provider.of<CheckBoxWidgetProvider>(context);
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            /// 뒤로가기 버튼
+            GestureDetector(
+              onTap: () {
+                context.go('/');
+              },
+              child: SvgPicture.asset(
+                'assets/image/left_arrow.svg',
+                width: 24.w,
+                height: 24.h,
+              ),
+            ).padding(top: 12.h),
+
+            /// 약관 동의 글자
+            PtdTextWidget.semiBold('약관동의', 36, MaeumGaGymColor.black)
+                .padding(top: 32.h),
+
+            /// 서비스 이용을 위해 필수 약관동의가 필요해요. 글자
+            PtdTextWidget.regular(
+                    '서비스 이용을 위해 필수 약관동의가 필요해요.', 16, MaeumGaGymColor.gray600)
+                .padding(top: 8.h),
+
+            /// 모두 동의해요. 위쪽 Divider
+            Divider(height: 2.h, color: MaeumGaGymColor.gray50)
+                .padding(top: 40.h),
+
+            /// 모두 동의해요. 위젯
+            SizedBox(
+              width: double.infinity,
+              height: 44.h,
+              child: Row(
+                children: [
+                  /// CheckBoxWidget ( allCheck )
+                  GestureDetector(
+                    onTap: () {
+                      click.clickAllCheck();
+                    },
+                    child: CheckBoxWidget(state: click.allCheck),
+                  ),
+
+                  /// 모두 동의해요. 글자
+                  PtdTextWidget.medium('모두 동의해요.', 20, MaeumGaGymColor.black)
+                      .padding(left: 12.w),
+                ],
+              ),
+            ).padding(top: 12.h, bottom: 12.h),
+
+            /// 모두 동의해요. 아래쪽 Divider
+            Divider(height: 2.h, color: MaeumGaGymColor.gray50),
+
+            /// 개인정보 수집 이용 동의 위젯
+            SizedBox(
+              width: double.infinity,
+              height: 44.h,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  /// CheckBoxWidget ( agreePersonInformation )
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          click.clickAgreePersonInformation();
+                          click.checkAll();
+                        },
+                        child:
+                            CheckBoxWidget(state: click.agreePersonInformation),
+                      ),
+
+                      /// 개인정보 수집 이용 동의 글자
+                      PtdTextWidget.regular(
+                              '개인정보 수집 이용 동의', 16, MaeumGaGymColor.black)
+                          .padding(left: 12.w),
+                    ],
+                  ),
+
+                  /// 자세히 보기 ( agreePersonInformation )
+                  PtdTextWidget.medium('자세히 보기', 14, MaeumGaGymColor.gray300)
+                      .textStyle(TextStyle(
+                    decoration: TextDecoration.underline,
+                    decorationColor: MaeumGaGymColor.gray300,
+                  )),
+                ],
+              ),
+            ).padding(top: 12.h, bottom: 8.h),
+
+            /// 이용 약관 동의 위젯
+            SizedBox(
+              width: double.infinity,
+              height: 44.h,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  /// CheckBoxWidget ( agreeConditionsOfUse )
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          click.clickAgreeConditionsOfUse();
+                          click.checkAll();
+                        },
+                        child:
+                            CheckBoxWidget(state: click.agreeConditionsOfUse),
+                      ),
+
+                      /// 이용 약관 동의 글자
+                      PtdTextWidget.regular(
+                              '이용 약관 동의', 16, MaeumGaGymColor.black)
+                          .padding(left: 12.w),
+                    ],
+                  ),
+
+                  /// 자세히 보기 ( agreeConditionsOfUse )
+                  PtdTextWidget.medium('자세히 보기', 14, MaeumGaGymColor.gray300)
+                      .textStyle(TextStyle(
+                    decoration: TextDecoration.underline,
+                    decorationColor: MaeumGaGymColor.gray300,
+                  )),
+                ],
+              ),
+            ).padding(top: 8.h, bottom: 8.h),
+
+            /// 만 14세 이상 위젯
+            SizedBox(
+              width: double.infinity,
+              height: 44.h,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  /// CheckBoxWidget ( moreThanAgeFourteen )
+                  GestureDetector(
+                    onTap: () {
+                      click.clickMoreThanAgeFourteen();
+                      click.checkAll();
+                    },
+                    child: CheckBoxWidget(state: click.moreThanAgeFourteen),
+                  ),
+
+                  /// 만 14세 이상 글자
+                  PtdTextWidget.regular('만 14세 이상', 16, MaeumGaGymColor.black)
+                      .padding(left: 12.w),
+                ],
+              ),
+            ).padding(top: 8.h, bottom: 8.h),
+
+            /// 마케팅 정보 수신 동의 위젯
+            SizedBox(
+              width: double.infinity,
+              height: 44.h,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  /// CheckBoxWidget ( agreeMarketingInformation )
+                  GestureDetector(
+                    onTap: () {
+                      click.clickAgreeMarketingInformation();
+                      click.checkAll();
+                    },
+                    child:
+                        CheckBoxWidget(state: click.agreeMarketingInformation),
+                  ),
+
+                  /// 마케팅 정보 수신 동의 글자
+                  PtdTextWidget.regular(
+                          '마케팅 정보 수신 동의 ', 16, MaeumGaGymColor.black)
+                      .padding(left: 12.w),
+
+                  PtdTextWidget.regular('(선택)', 16, MaeumGaGymColor.gray400),
+                ],
+              ),
+            ).padding(top: 8.h),
+
+            /// 공간을 위한
+            const Expanded(child: SizedBox()),
+
+            /// 확인 버튼
+            click.agreePersonInformation &&
+                    click.agreeConditionsOfUse &&
+                    click.moreThanAgeFourteen
+                ? CheckButton(
+                    width: 390,
+                    height: 58,
+                    color: MaeumGaGymColor.blue500,
+                    notUseRoute: false,
+                    route: '/',
+                    textWidget:
+                        PtdTextWidget.medium('확인', 20, MaeumGaGymColor.white),
+                  ).padding(bottom: 20.h)
+                : CheckButton(
+                    width: 390,
+                    height: 58,
+                    color: MaeumGaGymColor.gray400,
+                    notUseRoute: true,
+                    route: '',
+                    textWidget:
+                        PtdTextWidget.medium('확인', 20, MaeumGaGymColor.gray200),
+                  ).padding(bottom: 20.h)
+          ],
+        ).padding(left: 20.w, right: 20.w),
+      ),
+    );
+  }
+}
