84b6432f89dbd48f384424a6d9229e144d50e77f
ljyo2o9 <yggosxoljy@gmail.com>
2023-11-25 14:59:24 +0900

    🗒️ :: [#33] SignUpNickNameScreen 추가

---
+++ lib/presentation/signup_screen/signup_nickname_screen.dart
@@ -0,0 +1,19 @@
+/// Package
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:styled_widget/styled_widget.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+/// Core
+import 'package:wewith_flutter/core/component/text/pretendard/ptd_text_widget.dart';
+import 'package:wewith_flutter/core/component/check_botton.dart';
+import 'package:wewith_flutter/core/constant/maeumgagym_color.dart';
+
+class SignUpNickNameScreen extends StatelessWidget {
+  const SignUpNickNameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
