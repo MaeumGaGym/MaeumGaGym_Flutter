fff9d65429e52544d09f94e7da689c82d6935ad0
ljyo2o9 <yggosxoljy@gmail.com>
2023-11-25 17:41:21 +0900

    🗒️ :: [#33] SignUpNickNameScreen textField 추가하기 전

--- lib/presentation/signup_screen/signup_nickname_screen.dart
+++ lib/presentation/signup_screen/signup_nickname_screen.dart
@@ -1,6 +1,7 @@
 /// Package
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:styled_widget/styled_widget.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -14,6 +15,35 @@ class SignUpNickNameScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            /// 뒤로가기 버튼
+            GestureDetector(
+              onTap: () {
+                context.go('/signupAgree');
+              },
+              child: SvgPicture.asset(
+                'assets/image/left_arrow.svg',
+                width: 24.w,
+                height: 24.h,
+              ),
+            ).padding(top: 12.h),
+
+            /// 닉네임 글자
+            PtdTextWidget.semiBold('닉네임', 36, MaeumGaGymColor.black)
+                .padding(top: 32.h),
+
+            /// 서비스 이용을 위해 필수 약관동의가 필요해요. 글자
+            PtdTextWidget.regular(
+                    '자신만의 닉네임을 입력해 주세요', 16, MaeumGaGymColor.gray600)
+                .padding(top: 8.h),
+          ],
+        ).padding(left: 20.w, right: 20.w),
+      ),
+    );
   }
 }
