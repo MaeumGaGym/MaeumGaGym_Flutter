6814916c1c993e07e269f585881c64e844da25d8
ljyo2o9 <yggosxoljy@gmail.com>
2023-12-19 15:36:54 +0900

    🗒️ :: [#67] sign_up_nick_name_screen 추가

---
+++ lib/sign_up/presentation/sign_up_nick_name_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:styled_widget/styled_widget.dart';
+
+import '../../core/component/maeumgagym_text_field.dart';
+import '../../core/component/text/pretendard/ptd_text_widget.dart';
+
+import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
+
+import 'package:maeum_ga_gym_flutter/sign_up/widget/animated_check_button.dart';
+import 'package:maeum_ga_gym_flutter/sign_up/provider/nickname_text_field_riverpod.dart';
+
+class SignUpNickNmaeScreen extends ConsumerWidget {
+  const SignUpNickNmaeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var nicknameController = TextEditingController();
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            /// pop 버튼
+            Consumer(
+              builder: (context, ref, child) {
+                return GestureDetector(
+                  onTap: () {
+                    context.pop();
+                    ref.read(nicknameTextFieldProvider.notifier).clicked(false);
+                    ref.read(nicknameTextFieldProvider.notifier).isText(false);
+                  },
+                  child: SvgPicture.asset(
+                    'assets/image/left_arrow.svg',
+                    width: 24,
+                    height: 24,
+                  ),
+                ).padding(top: 12, left: 20, right: 20);
+              },
+            ),
+
+            /// 닉네임 글자
+            PtdTextWidget.semiBold('닉네임', 36, MaeumgagymColor.black)
+                .padding(top: 32, left: 20, right: 20),
+
+            /// 서비스 이용을 위해 필수 약관동의가 필요해요. 글자
+            PtdTextWidget.regular(
+                    '자신만의 닉네임을 입력해 주세요', 16, MaeumgagymColor.gray600)
+                .padding(top: 8, left: 20, right: 20),
+
+            /// 닉네임 TextField
+            MaeumgagymTextField(
+              controller: nicknameController,
+              text: '닉네임',
+              errorText: '이미 사용중인 닉네임이에요.',
+              textFieldProvider: nicknameTextFieldProvider,
+            ).padding(top: 32, left: 20, right: 20),
+          ],
+        ),
+      ),
+      bottomSheet: Consumer(
+        builder: (context, ref, child) {
+          return AnimatedCheckButton(
+            inText: ref.watch(nicknameTextFieldProvider).inText,
+            onClicked: ref.watch(nicknameTextFieldProvider).onClicked,
+            buttonProvider: nicknameTextFieldProvider,
+          );
+        },
+      ),
+    );
+  }
+}
+
+class MaeumGaGymColor {}
