849d0e7b7ecead4636754923f36c3d78d123a554
ljyo2o9 <yggosxoljy@gmail.com>
2024-01-04 22:46:39 +0900

    🗒️ :: [#83] TabBar에 들어갈 TabWidget 생성

---
+++ lib/pose/widget/tab_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import '../../config/maeumgagym_color.dart';
+import '../../core/component/text/pretendard/ptd_text_widget.dart';
+
+class TabWidget extends StatelessWidget {
+  final String text;
+
+  const TabWidget(this.text, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 15),
+        child: PtdTextWidget.labelLarge(text, MaeumgagymColor.black),
+      ),
+    );
+  }
+}
