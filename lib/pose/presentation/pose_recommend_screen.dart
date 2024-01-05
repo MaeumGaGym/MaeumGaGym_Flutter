60c633ba580df711815d68efbf1162d51dad8233
ljyo2o9 <yggosxoljy@gmail.com>
2024-01-05 09:22:23 +0900

    🗒️ :: [#83] PoseRecommendScreen(추천 운동 페이지)

---
+++ lib/pose/presentation/pose_recommend_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:maeum_ga_gym_flutter/pose/widget/pose_recommend_widget.dart';
+
+import 'package:maeum_ga_gym_flutter/pose/widget/pose_data.dart';
+
+class PoseReCommendScreen extends StatelessWidget {
+  final TabController tabContoller;
+
+  const PoseReCommendScreen({required this.tabContoller, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child: Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          /// 맨몸 운동
+          PoseRecommendWidget(
+            tabController: tabContoller,
+            data: data,
+            keyData: '맨몸',
+            titleImage: 'assets/image/pose_icon/pose_arm.png',
+            index: 1,
+          ),
+
+          /// 기구 운동
+          PoseRecommendWidget(
+            tabController: tabContoller,
+            data: data,
+            keyData: '기구',
+            titleImage: 'assets/image/pose_icon/pose_machine.png',
+            index: 2,
+          ),
+        ],
+      ),
+    ));
+  }
+}
