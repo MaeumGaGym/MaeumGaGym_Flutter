a71165783be30439faa05281af65d8b80373e815
iyes826 <treeyu12345@naver.com>
2024-01-08 22:04:19 +0900

    🍗 :: [#99] SafeArea 적용

--- lib/self_care/presentation/widget/profile/container/self_care_profile_main_title_container.dart
+++ lib/self_care/presentation/widget/profile/container/self_care_profile_main_title_container.dart
@@ -7,19 +7,21 @@ class SelfCareProfileMainTitleContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        PtdTextWidget.titleLarge(
-          "내 프로필",
-          MaeumgagymColor.black,
-        ),
-        const SizedBox(height: 12),
-        PtdTextWidget.bodyMedium(
-          "내 프로필을 관리해보세요.",
-          MaeumgagymColor.gray600,
-        ),
-      ],
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          PtdTextWidget.titleLarge(
+            "내 프로필",
+            MaeumgagymColor.black,
+          ),
+          const SizedBox(height: 12),
+          PtdTextWidget.bodyMedium(
+            "내 프로필을 관리해보세요.",
+            MaeumgagymColor.gray600,
+          ),
+        ],
+      ),
     );
   }
 }
