f517ad95889ccf0b450911b0849c6bbc16a8c084
iyes826 <treeyu12345@naver.com>
2024-01-08 22:19:46 +0900

    ⚓️ :: SelfCare Profile Setting Item dummy 데이터 추가

--- lib/self_care/presentation/widget/self_care_dummy.dart
+++ lib/self_care/presentation/widget/self_care_dummy.dart
@@ -4,3 +4,9 @@ final Map<String, String> selfCareWidgetItemList = {
   "assets/image/self_care_icon/protein_shake_icon.svg" : "식단",
   "assets/image/self_care_icon/gallery_icon.svg" : "오운완",
 };
+
+final List<String> selfCareProfileSettingItemList = [
+  "내 정보 변경",
+  "로그아웃",
+  "회원탈퇴",
+];
