5d4c02ace011801db0e04527562cb4f200aa6167
iyes826 <treeyu12345@naver.com>
2024-01-08 21:37:04 +0900

    🗒️ :: [#99] SelfCare Default AppBar 추가

---
+++ lib/self_care/presentation/widget/self_care_default_app_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
+
+class SelfCareDefaultAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
+  final String? title;
+  final String iconPath;
+
+  const SelfCareDefaultAppBar({
+    Key? key,
+    this.title,
+    required this.iconPath,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: PreferredSize(
+        preferredSize: preferredSize,
+        child: AppBar(
+          backgroundColor: MaeumgagymColor.white,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  alignment: Alignment.center,
+                  child: SvgPicture.asset(
+                    iconPath,
+                    width: 28,
+                    height: 28,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(52);
+}
