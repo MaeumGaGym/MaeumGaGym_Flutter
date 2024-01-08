ef8b03a185ba39f9f4a9a956bd9c94e7850b9c18
iyes826 <treeyu12345@naver.com>
2024-01-08 22:41:05 +0900

    🍗 :: width 사이즈 수치 수정

--- lib/self_care/presentation/widget/profile/container/self_care_profile_main_info_widget_container.dart
+++ lib/self_care/presentation/widget/profile/container/self_care_profile_main_info_widget_container.dart
@@ -5,9 +5,9 @@ class SelfCareProfileMainInfoWidgetContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: SizedBox( // 이거 디자인 확정안됬다고 해서 나중에 다시 추가함
-        width: 390,
+        width: MediaQuery.of(context).size.width,
         height: 295,
         child: Placeholder(),
       ),
