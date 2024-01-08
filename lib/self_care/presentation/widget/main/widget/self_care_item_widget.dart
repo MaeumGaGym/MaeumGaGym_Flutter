cea39a4b8d20bef09168b762d77ece24445ef8b8
iyes826 <treeyu12345@naver.com>
2024-01-08 11:17:23 +0900

    ⚓️ :: [#80] SelfCare 메인 화면에서 쓰일 위젯 item화

---
+++ lib/self_care/presentation/widget/main/widget/self_care_item_widget.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
+import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
+
+class SelfCareItemWidget extends StatelessWidget {
+  final String imagePath;
+  final String title;
+
+  const SelfCareItemWidget({
+    Key? key,
+    required this.imagePath,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              GestureDetector(
+                child: Container(
+                  height: 40,
+                  width: 40,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: MaeumgagymColor.gray200,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: SvgPicture.asset(
+                    imagePath,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16),
+              PtdTextWidget.bodyLarge(
+                title,
+                MaeumgagymColor.black,
+              ),
+            ],
+          ),
+          SvgPicture.asset(
+            "assets/image/core_icon/right_arrow_icon.svg",
+            width: 24,
+            height: 24,
+          ),
+        ],
+      ),
+    );
+  }
+}
