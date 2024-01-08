9ee36d3c2b9c58a76a8e101c8071202ccf3be934
iyes826 <treeyu12345@naver.com>
2024-01-08 11:14:29 +0900

    🗒️ :: [#80] SelfCare 타임라인 파일 추가

---
+++ lib/self_care/presentation/widget/main/container/self_care_main_time_line_container.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SelfCareMainTimerLineContainer extends StatelessWidget {
+  const SelfCareMainTimerLineContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 24),
+      child: Container(
+
+
+      ),
+    );
+  }
+}
