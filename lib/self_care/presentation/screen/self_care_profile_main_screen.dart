49b9339da2b1db092de49ed5c17e2b551e2b44a1
iyes826 <treeyu12345@naver.com>
2024-01-08 21:38:26 +0900

    ⚓️ :: [#99] SelfCare Default AppBar 적용

---
+++ lib/self_care/presentation/screen/self_care_profile_main_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/profile/container/self_care_profile_main_info_widget_container.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/profile/container/self_care_profile_main_title_container.dart';
+
+import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/self_care_default_app_bar.dart';
+
+class SelfCareProfileMainScreen extends StatelessWidget {
+  const SelfCareProfileMainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: MaeumgagymColor.white,
+      appBar: SelfCareDefaultAppBar(
+        iconPath: "assets/image/core_icon/left_arrow_icon.svg",
+      ),
+    );
+  }
+}
