70f16a1f03e72bfba808c754226234dedfbb1e60
iyes826 <treeyu12345@naver.com>
2024-01-08 22:05:23 +0900

    🍗 :: [#99] SingleChildScrollView 적용

--- lib/self_care/presentation/screen/self_care_profile_main_screen.dart
+++ lib/self_care/presentation/screen/self_care_profile_main_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/profile/container/self_care_profile_main_info_widget_container.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/profile/container/self_care_profile_main_setting_container.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/profile/container/self_care_profile_main_title_container.dart';
 
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/self_care_default_app_bar.dart';
@@ -16,16 +17,20 @@ class SelfCareProfileMainScreen extends StatelessWidget {
       appBar: SelfCareDefaultAppBar(
         iconPath: "assets/image/core_icon/left_arrow_icon.svg",
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 24),
-            SelfCareProfileMainTitleContainer(),
-            SizedBox(height: 32),
-            SelfCareProfileMainInfoWidgetContainer(),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 24),
+              SelfCareProfileMainTitleContainer(),
+              SizedBox(height: 32),
+              SelfCareProfileMainInfoWidgetContainer(),
+              SizedBox(height: 32),
+              SelfCareProfileMainSettingContainer(),
+            ],
+          ),
         ),
       ),
     );
