90dafe7ab0b5c24620053497323d878ea2320c04
iyes826 <treeyu12345@naver.com>
2024-01-08 21:31:44 +0900

    🗒️ :: [#99] SelfCare Profile Main Title Container 추가

---
+++ lib/self_care/presentation/widget/profile/container/self_care_profile_main_info_widget_container.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SelfCareProfileMainInfoWidgetContainer extends StatelessWidget {
+  const SelfCareProfileMainInfoWidgetContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
