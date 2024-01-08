699b4ce3fa4377686263033c4ed3d6926f9f6657
iyes826 <treeyu12345@naver.com>
2024-01-08 22:27:15 +0900

    🍗 :: 위젯 상수 처리

--- lib/self_care/presentation/widget/profile/container/self_care_profile_main_info_widget_container.dart
+++ lib/self_care/presentation/widget/profile/container/self_care_profile_main_info_widget_container.dart
@@ -5,11 +5,11 @@ class SelfCareProfileMainInfoWidgetContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container( // 이거 디자인 확정안됬다고 해서 나중에 다시 추가함
+    return const SafeArea(
+      child: SizedBox( // 이거 디자인 확정안됬다고 해서 나중에 다시 추가함
         width: 390,
         height: 295,
-        child: const Placeholder(),
+        child: Placeholder(),
       ),
     );
   }
