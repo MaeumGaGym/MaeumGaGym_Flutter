40537e41806be039ee4db0e500cb83d870790520
ljyo2o9 <yggosxoljy@gmail.com>
2024-01-09 15:25:08 +0900

    🗒️ :: [#126] CalorieCalculatorAppBar 생성

---
+++ lib/calorie_calculator/presentation/widget/main/calorie_calculator_app_bar.dart
@@ -0,0 +1,20 @@
+/// Package
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+/// Core
+import '../../../../config/maeumgagym_color.dart';
+import '../../../../core/component/text/pretendard/ptd_text_widget.dart';
+
+class CalorieCalculatorAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
+  const CalorieCalculatorAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(48);
+}
