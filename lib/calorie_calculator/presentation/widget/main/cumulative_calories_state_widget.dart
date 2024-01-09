bc14646ff2f9fd769009cf2b445d2a4f57a6e8f8
ljyo2o9 <yggosxoljy@gmail.com>
2024-01-09 15:45:23 +0900

    🗒️ :: [#128] CumulativeCaloriesStateWidget View 완성

--- lib/calorie_calculator/presentation/widget/main/cumulative_calories_state_widget.dart
+++ lib/calorie_calculator/presentation/widget/main/cumulative_calories_state_widget.dart
@@ -10,6 +10,29 @@ class CumulativeCaloriesStateWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      height: 52,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            PtdTextWidget.labelLarge('누적 칼로리', MaeumgagymColor.black),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                PtdTextWidget.titleMedium("1,080", MaeumgagymColor.black),
+                PtdTextWidget.labelMedium(
+                  ' / 1,774kcal',
+                  MaeumgagymColor.gray300,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
