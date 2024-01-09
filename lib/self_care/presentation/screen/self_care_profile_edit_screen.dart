f32dd7e88b40ad72b0115743a4cdb58762969132
iyes826 <treeyu12345@naver.com>
2024-01-09 12:18:23 +0900

    ⚓️ :: [#121] SelfCareDefaultAppBar 적용

--- lib/self_care/presentation/screen/self_care_profile_edit_screen.dart
+++ lib/self_care/presentation/screen/self_care_profile_edit_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/self_care_default_app_bar.dart';
 
 class SelfCareProfileEditScreen extends StatelessWidget {
   const SelfCareProfileEditScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: MaeumgagymColor.white,
+      appBar: SelfCareDefaultAppBar(
+        iconPath: "assets/image/core_icon/left_arrow_icon.svg",
+        title: "내 정보 변경",
+      ),
+    );
   }
 }
