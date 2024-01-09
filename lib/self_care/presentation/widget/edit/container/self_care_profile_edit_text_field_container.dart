7ea59ebe1121ea5fbaa7a5b8fe01be3ca4786693
iyes826 <treeyu12345@naver.com>
2024-01-09 17:29:49 +0900

    ⚓️ :: [#128] SelfCareProfileTextFileld 위젯화

--- lib/self_care/presentation/widget/edit/container/self_care_profile_edit_text_field_container.dart
+++ lib/self_care/presentation/widget/edit/container/self_care_profile_edit_text_field_container.dart
@@ -65,39 +65,7 @@ class _SelfCareProfileEditTextFiledContainerState
           MaeumgagymColor.black,
         ),
         const SizedBox(height: 8),
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: 48,
-          decoration: BoxDecoration(
-            color: _nameNode.hasFocus
-                ? MaeumgagymColor.blue50
-                : MaeumgagymColor.gray25,
-            borderRadius: BorderRadius.circular(8),
-            border: Border.all(
-              width: 1,
-              color: _nameNode.hasFocus ? MaeumgagymColor.blue100 : MaeumgagymColor.gray50,
-            ),
-          ),
-          child: Center(
-            child: TextFormField(
-              controller: _nameController,
-              focusNode: _nameNode,
-              style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w400,
-                fontFamily: "Pretendard",
-                overflow: TextOverflow.visible,
-                decorationThickness: 0,
-              ),
-              decoration: const InputDecoration(
-                border: InputBorder.none,
-                isCollapsed: true,
-                contentPadding: EdgeInsets.symmetric(horizontal: 12),
-              ),
-              cursorColor: MaeumgagymColor.blue600,
-            ),
-          ),
-        ),
+        SelfCareTextField(nameNode: _nameNode, nameController: _nameController),
       ],
     );
   }
