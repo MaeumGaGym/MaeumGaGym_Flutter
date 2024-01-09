9e6c638d4c8b4d6ae366d43f690d0f57ee9a1e90
iyes826 <treeyu12345@naver.com>
2024-01-09 10:20:09 +0900

    🚫 :: dummy 데이터에 저장된 Function 기능 삭제

--- lib/self_care/presentation/widget/self_care_dummy.dart
+++ lib/self_care/presentation/widget/self_care_dummy.dart
@@ -1,12 +1,9 @@
-final Map<String, String> selfCareWidgetItemList = {
-  "assets/image/self_care_icon/routine_icon.svg" : "내 루틴",
-  "assets/image/self_care_icon/objective_icon.svg" : "목표",
-  "assets/image/self_care_icon/protein_shake_icon.svg" : "식단",
-  "assets/image/self_care_icon/gallery_icon.svg" : "오운완",
-};
+import 'package:flutter/material.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/profile/widget/dialog/self_care_profile_quit_dialog.dart';
 
-final List<String> selfCareProfileSettingItemList = [
-  "내 정보 변경",
-  "로그아웃",
-  "회원탈퇴",
-];
+final Map<String, String> selfCareWidgetItemList = {
+  "assets/image/self_care_icon/routine_icon.svg": "내 루틴",
+  "assets/image/self_care_icon/objective_icon.svg": "목표",
+  "assets/image/self_care_icon/protein_shake_icon.svg": "식단",
+  "assets/image/self_care_icon/gallery_icon.svg": "오운완",
+};
