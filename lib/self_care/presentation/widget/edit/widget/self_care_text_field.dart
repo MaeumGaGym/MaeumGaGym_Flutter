8a1bf075afdcbbf6f9447ab49d60ce9ea28c70da
iyes826 <treeyu12345@naver.com>
2024-01-09 18:08:14 +0900

    💉 :: [#128] 매개변수 수정

--- lib/self_care/presentation/widget/edit/widget/self_care_text_field.dart
+++ lib/self_care/presentation/widget/edit/widget/self_care_text_field.dart
@@ -1,50 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
+import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
 
-class SelfCareTextField extends StatelessWidget {
+class SelfCareTextField extends StatefulWidget {
+  final String title;
+  final FocusNode focusNode;
+  final TextEditingController controller;
+
   const SelfCareTextField({
     super.key,
-    required FocusNode nameNode,
-    required TextEditingController nameController,
-  }) : _nameNode = nameNode, _nameController = nameController;
-
-  final FocusNode _nameNode;
-  final TextEditingController _nameController;
+    required this.title,
+    required this.focusNode,
+    required this.controller,
+  });
 
   @override
+  State<SelfCareTextField> createState() => _SelfCareTextFieldState();
+}
+
+class _SelfCareTextFieldState extends State<SelfCareTextField> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 48,
-      decoration: BoxDecoration(
-        color: _nameNode.hasFocus
-            ? MaeumgagymColor.blue50
-            : MaeumgagymColor.gray25,
-        borderRadius: BorderRadius.circular(8),
-        border: Border.all(
-          width: 1,
-          color: _nameNode.hasFocus ? MaeumgagymColor.blue100 : MaeumgagymColor.gray50,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        PtdTextWidget.bodyMedium(
+          widget.title,
+          MaeumgagymColor.black,
         ),
-      ),
-      child: Center(
-        child: TextFormField(
-          controller: _nameController,
-          focusNode: _nameNode,
-          style: const TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w400,
-            fontFamily: "Pretendard",
-            overflow: TextOverflow.visible,
-            decorationThickness: 0,
+        const SizedBox(height: 8),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: 48,
+          decoration: BoxDecoration(
+            color: widget.focusNode.hasFocus
+                ? MaeumgagymColor.blue50
+                : MaeumgagymColor.gray25,
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(
+              width: 1,
+              color: widget.focusNode.hasFocus
+                  ? MaeumgagymColor.blue100
+                  : MaeumgagymColor.gray50,
+            ),
           ),
-          decoration: const InputDecoration(
-            border: InputBorder.none,
-            isCollapsed: true,
-            contentPadding: EdgeInsets.symmetric(horizontal: 12),
+          child: Center(
+            child: TextFormField(
+              controller: widget.controller,
+              focusNode: widget.focusNode,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+                fontFamily: "Pretendard",
+                overflow: TextOverflow.visible,
+                decorationThickness: 0,
+              ),
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                isCollapsed: true,
+                contentPadding: EdgeInsets.symmetric(horizontal: 12),
+              ),
+              cursorColor: MaeumgagymColor.blue600,
+            ),
           ),
-          cursorColor: MaeumgagymColor.blue600,
         ),
-      ),
+      ],
     );
   }
 }
-
