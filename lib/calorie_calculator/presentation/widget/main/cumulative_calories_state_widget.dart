105fee6ed0d8567392a49f60bf1a692756e1e897
ljyo2o9 <yggosxoljy@gmail.com>
2024-01-09 15:43:15 +0900

    🗒️ :: [#128] CumulativeCaloriesStateWidget 생성

---
+++ lib/calorie_calculator/presentation/widget/main/cumulative_calories_state_widget.dart
@@ -0,0 +1,15 @@
+/// Package
+import 'package:flutter/material.dart';
+
+/// Core
+import '../../../../config/maeumgagym_color.dart';
+import '../../../../core/component/text/pretendard/ptd_text_widget.dart';
+
+class CumulativeCaloriesStateWidget extends StatelessWidget {
+  const CumulativeCaloriesStateWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
