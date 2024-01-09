236162b0cec59524c5764862ec1bdafa5571a0b7
ljyo2o9 <yggosxoljy@gmail.com>
2024-01-09 15:47:14 +0900

    🗒️ :: [#128] CumulativeCaloriesBarWidget 생성

---
+++ lib/calorie_calculator/presentation/widget/main/cumulative_caloriees_bar_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../../../../config/maeumgagym_color.dart';
+
+class CumulativeCaloriesBarWidget extends StatelessWidget {
+  const CumulativeCaloriesBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
