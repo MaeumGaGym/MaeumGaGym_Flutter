4fbaa8e64ac7a0c8982a556c9ec4ec68e40c9768
iyes826 <treeyu12345@naver.com>
2024-01-10 09:58:59 +0900

    ⚓️ :: [#141] SelfCareMainScreen을 Page Manager에 적용

--- lib/page_manager/presentation/view/page_manager_screen.dart
+++ lib/page_manager/presentation/view/page_manager_screen.dart
@@ -5,6 +5,7 @@ import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
 import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
 import 'package:maeum_ga_gym_flutter/home/presentation/view/home_main_screen.dart';
 import 'package:maeum_ga_gym_flutter/page_manager/presentation/view/animated_indexed_stack_screen.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/view/self_care_main_screen.dart';
 import '../../../pose/presentation/view/pose_tab_screen.dart';
 import '../provider/page_manager_controller.dart';
 
@@ -21,7 +22,7 @@ class PageManagerScreen extends ConsumerWidget {
       const PoseTabScreen(),
       Container(color: Colors.red),
       Container(color: Colors.blue),
-      Container(color: Colors.green),
+      const SelfCareMainScreen(),
     ];
 
     return Scaffold(
