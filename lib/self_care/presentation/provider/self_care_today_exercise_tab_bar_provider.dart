06e0a2919d353d01c1dc4aa2f628795d9ee01d7a
iyes826 <treeyu12345@naver.com>
2024-01-10 17:19:55 +0900

    ⚓️ :: [#159] ExerciseTabBarNotifier 추가

--- lib/self_care/presentation/provider/self_care_today_exercise_tab_bar_provider.dart
+++ lib/self_care/presentation/provider/self_care_today_exercise_tab_bar_provider.dart
@@ -1,4 +1,22 @@
-enum TodayExerciseTabBarState {
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final exerciseTabBarNotifierProvider = StateNotifierProvider<ExerciseTabBarNotifier, ExerciseTabBarState>(
+  (ref) => ExerciseTabBarNotifier(),
+);
+
+enum ExerciseTabBarState {
   camera,
   gallery,
+}
+
+class ExerciseTabBarNotifier extends StateNotifier<ExerciseTabBarState> {
+  ExerciseTabBarNotifier() : super(ExerciseTabBarState.camera);
+
+  void changeCamera() {
+    state = ExerciseTabBarState.camera;
+  }
+
+  void changeGallery() {
+    state = ExerciseTabBarState.gallery;
+  }
 }
