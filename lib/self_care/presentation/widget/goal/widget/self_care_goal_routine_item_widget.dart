7b7c7f5c3061085e58f8d30e3774ae4e79343db8
iyes826 <treeyu12345@naver.com>
2024-01-11 01:18:57 +0900

    ⚓️ :: [#172] SelfCareGoalSharedWidget 적용

--- lib/self_care/presentation/widget/goal/widget/self_care_goal_routine_item_widget.dart
+++ lib/self_care/presentation/widget/goal/widget/self_care_goal_routine_item_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
 import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/goal/widget/self_care_goal_shared_widget.dart';
 
 class SelfCareGoalRoutineItemWidget extends StatelessWidget {
   const SelfCareGoalRoutineItemWidget({Key? key}) : super(key: key);
@@ -40,26 +41,9 @@ class SelfCareGoalRoutineItemWidget extends StatelessWidget {
                     color: MaeumgagymColor.white,
                     borderRadius: BorderRadius.circular(100),
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    child: Row(
-                      children: [
-                        PtdTextWidget.labelMedium(
-                          "공유됨",
-                          MaeumgagymColor.blue500,
-                        ),
-                        SizedBox(width: 8),
-                        SvgPicture.asset(
-                          "assets/image/self_care_icon/earth_icon.svg",
-                          width: 16,
-                          height: 16,
-                          color: MaeumgagymColor.blue500,
-                        ),
-                      ],
-                    ),
-                  ),
+                  child: SelfCareGoalSharedWidget(),
                 ),
-                SizedBox(width: 12),
+                const SizedBox(width: 12),
                 SvgPicture.asset(
                   "assets/image/core_icon/dots_vertical_icon.svg",
                   color: MaeumgagymColor.gray400,
