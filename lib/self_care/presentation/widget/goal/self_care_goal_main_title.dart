45db9130837a4b4fc4ef39ffb86b883840c3bac1
iyes826 <treeyu12345@naver.com>
2024-01-11 00:34:58 +0900

    🗒️ :: [#172] SelfCareGoalMainTitle 추가

---
+++ lib/self_care/presentation/widget/goal/self_care_goal_main_title.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
+import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
+
+class SelfCareGoalMainTitle extends StatelessWidget {
+  const SelfCareGoalMainTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        PtdTextWidget.titleLarge(
+          "내 루틴",
+          MaeumgagymColor.black,
+        ),
+        SizedBox(height: 12),
+        PtdTextWidget.bodyMedium(
+          "나만의 루틴을 구성하여\n규칙적인 운동을 해보세요.",
+          MaeumgagymColor.gray600,
+        ),
+      ],
+    );
+  }
+}
