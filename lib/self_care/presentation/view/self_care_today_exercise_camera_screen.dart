629abedc186f7e4482106e89cd3c39286a051abb
iyes826 <treeyu12345@naver.com>
2024-01-10 22:02:48 +0900

    💉 :: [#159] 주석 추가

--- lib/self_care/presentation/view/self_care_today_exercise_camera_screen.dart
+++ lib/self_care/presentation/view/self_care_today_exercise_camera_screen.dart
@@ -5,6 +5,7 @@ class SelfCareTodayExerciseCameraScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    /// 이건 새로 이슈 파서 진행할겁니다.
     return const Placeholder();
   }
 }
