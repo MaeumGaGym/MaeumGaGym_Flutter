b51b8f2754708ea82e64c268f9feed3e30433839
iyes826 <treeyu12345@naver.com>
2024-01-11 00:35:24 +0900

    ⚓️ :: [#172] SelfCareGoalMainTitle 적용

--- lib/self_care/presentation/view/self_care_goal_main_screen.dart
+++ lib/self_care/presentation/view/self_care_goal_main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/goal/self_care_goal_main_title.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/self_care_default_app_bar.dart';
 
 class SelfCareGoalMainScreen extends StatelessWidget {
@@ -9,6 +10,15 @@ class SelfCareGoalMainScreen extends StatelessWidget {
     return Scaffold(
       appBar: SelfCareDefaultAppBar(
         iconPath: "assets/image/core_icon/left_arrow_icon.svg",
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SelfCareGoalMainTitle(),
+          ],
+        ),
       ),
     );
   }
