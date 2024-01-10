cf5c12edefdabd15ddfa668e769aca8acfdd8d3d
iyes826 <treeyu12345@naver.com>
2024-01-10 21:30:41 +0900

    💉 :: [#159] TabBar 설정

--- lib/self_care/presentation/view/self_care_today_exercise_main_screen.dart
+++ lib/self_care/presentation/view/self_care_today_exercise_main_screen.dart
@@ -1,47 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
+import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/self_care_default_app_bar.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/today_exercise/self_care_today_exercise_fab.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/today_exercise/self_care_today_exercise_main_title.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/today_exercise/self_care_today_exercise_page_container.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/today_exercise/self_care_today_exercise_tab_bar.dart';
 
 class SelfCareTodayExerciseMainScreen extends StatelessWidget {
   const SelfCareTodayExerciseMainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: MaeumgagymColor.white,
-      appBar: const SelfCareDefaultAppBar(
-        iconPath: "assets/image/core_icon/left_arrow_icon.svg",
-      ),
-      body: const SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SelfCareTodayExerciseMainTitle(),
-            ],
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: MaeumgagymColor.white,
+        appBar: const SelfCareDefaultAppBar(
+          iconPath: "assets/image/core_icon/left_arrow_icon.svg",
+        ),
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SelfCareTodayExerciseMainTitle(),
+                SelfCareTodayExercisePageContainer(),
+              ],
+            ),
           ),
         ),
+        bottomNavigationBar: const SelfCareTodayExerciseTabBar(),
+        floatingActionButton: const SelfCareTodayExerciseFAB(),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       ),
-      floatingActionButton: SizedBox(
-        height: 64,
-        width: 64,
-        child: FloatingActionButton(
-          onPressed: () {},
-          shape: const CircleBorder(),
-          backgroundColor: MaeumgagymColor.blue500,
-          heroTag: "cameraButton",
-          child: SvgPicture.asset(
-            "assets/image/self_care_icon/camera_icon.svg",
-            color: MaeumgagymColor.white,
-            width: 40,
-            height: 40,
-          ),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
