392afd4fbc6c746703714b4e714433c234e8bcae
iyes826 <treeyu12345@naver.com>
2024-01-11 08:39:02 +0900

    ⚓️ :: [#172] SelfCareGoalRoutineItemWidget 적용

--- lib/self_care/presentation/view/self_care_goal_main_screen.dart
+++ lib/self_care/presentation/view/self_care_goal_main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/goal/self_care_goal_main_title.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/goal/widget/self_care_goal_main_title.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/goal/widget/self_care_goal_routine_item_widget.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/self_care_default_app_bar.dart';
 
 class SelfCareGoalMainScreen extends StatelessWidget {
@@ -7,16 +8,18 @@ class SelfCareGoalMainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       appBar: SelfCareDefaultAppBar(
         iconPath: "assets/image/core_icon/left_arrow_icon.svg",
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 24),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SelfCareGoalMainTitle(),
+            SizedBox(height: 32),
+            SelfCareGoalRoutineItemWidget(title: "주말 루틴"),
           ],
         ),
       ),
