27b3d0bffabfa09d75719587264be7af11a50131
iyes826 <treeyu12345@naver.com>
2024-01-10 21:26:48 +0900

    🗒️ :: [#159] SelfCareTodayExerciseCameraScreen 추가

---
+++ lib/self_care/presentation/view/self_care_today_exercise_camera_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SelfCareTodayExerciseCameraScreen extends StatelessWidget {
+  const SelfCareTodayExerciseCameraScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
