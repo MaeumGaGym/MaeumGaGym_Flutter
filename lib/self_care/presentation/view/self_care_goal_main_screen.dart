258459012409c1c36b92f1f3fc65f186e3c871a1
iyes826 <treeyu12345@naver.com>
2024-01-11 00:22:12 +0900

    🗒️ :: [#172] SelfCareGoalMainScreen 추가

---
+++ lib/self_care/presentation/view/self_care_goal_main_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SelfCareGoalMainScreen extends StatelessWidget {
+  const SelfCareGoalMainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
