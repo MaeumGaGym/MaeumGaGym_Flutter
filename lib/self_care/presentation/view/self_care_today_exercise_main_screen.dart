fdc9be504ed1bcfd701551c9f1250236a81abfdc
iyes826 <treeyu12345@naver.com>
2024-01-10 14:21:34 +0900

    ⚓️ :: [#147] SelfCareTodayExerciseMainTitle 적용

--- lib/self_care/presentation/view/self_care_today_exercise_main_screen.dart
+++ lib/self_care/presentation/view/self_care_today_exercise_main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/self_care_default_app_bar.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/today_exercise/self_care_today_exercise_main_title.dart';
 
 class SelfCareTodayExerciseMainScreen extends StatelessWidget {
   const SelfCareTodayExerciseMainScreen({Key? key}) : super(key: key);
@@ -11,6 +12,17 @@ class SelfCareTodayExerciseMainScreen extends StatelessWidget {
       backgroundColor: MaeumgagymColor.white,
       appBar: SelfCareDefaultAppBar(
         iconPath: "assets/image/core_icon/left_arrow_icon.svg",
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SelfCareTodayExerciseMainTitle(),
+            ],
+          ),
+        ),
       ),
     );
   }
