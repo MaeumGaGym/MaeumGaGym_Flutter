c66b7519840616998dd4b83fa79b9aef6f1d0f38
iyes826 <treeyu12345@naver.com>
2024-01-10 22:06:06 +0900

    💉 :: [#159] 주석 추가

--- lib/self_care/presentation/provider/self_care_today_exercise_tab_bar_provider.dart
+++ lib/self_care/presentation/provider/self_care_today_exercise_tab_bar_provider.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final exerciseTabBarNotifierProvider =
-    StateNotifierProvider<ExerciseTabBarNotifier, ExerciseTabBarState>(
+/// TabBar 상태를 관리
+final exerciseTabBarNotifierProvider = StateNotifierProvider<ExerciseTabBarNotifier, ExerciseTabBarState>(
   (ref) => ExerciseTabBarNotifier(),
 );
 
+/// PageView를 관리하는 Controller를 provider로 변환함.
 final exercisePageControllerProvider = StateProvider<PageController>(
   (ref) => PageController(initialPage: 0),
 );
 
+/// TabBar 상태
 enum ExerciseTabBarState {
   camera,
   gallery,
 }
 
+/// Notifier로 바로바로 변경되도록 구현
 class ExerciseTabBarNotifier extends StateNotifier<ExerciseTabBarState> {
   ExerciseTabBarNotifier() : super(ExerciseTabBarState.camera);
 
