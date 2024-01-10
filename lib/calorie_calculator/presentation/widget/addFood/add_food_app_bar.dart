cd122a37486c51a4c6b5d22e50e3f743d8493e7a
ljyo2o9 <yggosxoljy@gmail.com>
2024-01-11 00:41:54 +0900

    🗒️ :: [#170] PreferredSize 추가

--- lib/calorie_calculator/presentation/widget/addFood/add_food_app_bar.dart
+++ lib/calorie_calculator/presentation/widget/addFood/add_food_app_bar.dart
@@ -10,31 +10,34 @@ class AddFoodAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      centerTitle: true,
-      automaticallyImplyLeading: false,
-      title: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            /// 그냥 뒤로가기 버튼
-            Image.asset(
-              'assets/image/core_icon/close_icon.png',
-              width: 16,
-              height: 16,
-            ),
+    return PreferredSize(
+      preferredSize: preferredSize,
+      child: AppBar(
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              /// 그냥 뒤로가기 버튼
+              Image.asset(
+                'assets/image/core_icon/close_icon.png',
+                width: 16,
+                height: 16,
+              ),
 
-            /// Title
-            PtdTextWidget.labelLarge('음식 추가', MaeumgagymColor.black),
+              /// Title
+              PtdTextWidget.labelLarge('음식 추가', MaeumgagymColor.black),
 
-            /// 변경 저장하고 뒤로가기
-            Image.asset(
-              'assets/image/core_icon/check_icon.png',
-              width: 21.33,
-              height: 13.33,
-            ),
-          ],
+              /// 변경 저장하고 뒤로가기
+              Image.asset(
+                'assets/image/core_icon/check_icon.png',
+                width: 21.33,
+                height: 13.33,
+              ),
+            ],
+          ),
         ),
       ),
     );
