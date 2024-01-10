2781e24ad9f6ead527a764b05b664195ec3d2667
iyes826 <treeyu12345@naver.com>
2024-01-10 13:03:21 +0900

    🚫 :: [#147] SelfCare Dummy 데이터 삭제

--- lib/self_care/presentation/widget/self_care_dummy.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/profile/widget/dialog/self_care_profile_quit_dialog.dart';
-
-final Map<String, String> selfCareWidgetItemList = {
-  "assets/image/self_care_icon/routine_icon.svg": "내 루틴",
-  "assets/image/self_care_icon/objective_icon.svg": "목표",
-  "assets/image/self_care_icon/protein_shake_icon.svg": "식단",
-  "assets/image/self_care_icon/gallery_icon.svg": "오운완",
-};
