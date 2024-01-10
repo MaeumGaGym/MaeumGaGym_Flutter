41ab6d4684d3007c4a74eb80157345a8d3b87ba4
iyes826 <treeyu12345@naver.com>
2024-01-10 12:59:21 +0900

    🗒️ :: [#147] SelfCareTodayExerciseMainScreen 추가

---
+++ lib/self_care/presentation/view/self_care_today_exercise_main_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/self_care_default_app_bar.dart';
+
+class SelfCareTodayExerciseMainScreen extends StatelessWidget {
+  const SelfCareTodayExerciseMainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
