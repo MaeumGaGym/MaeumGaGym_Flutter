59b7f284531dff59b9ebfb025d09d03d88133977
iyes826 <treeyu12345@naver.com>
2024-01-10 22:02:38 +0900

    💉 :: [#159] 주석 추가

--- lib/self_care/presentation/view/self_care_today_exercise_main_screen.dart
+++ lib/self_care/presentation/view/self_care_today_exercise_main_screen.dart
@@ -8,6 +8,7 @@ import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/today_exercis
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/today_exercise/self_care_today_exercise_page_container.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/today_exercise/self_care_today_exercise_tab_bar.dart';
 
+/// stless로 하면 PageView 부분에서 에러나서 stful로 했습니다.
 class SelfCareTodayExerciseMainScreen extends StatefulWidget {
   const SelfCareTodayExerciseMainScreen({Key? key}) : super(key: key);
 
@@ -29,15 +30,20 @@ class _SelfCareTodayExerciseMainScreenState extends State<SelfCareTodayExerciseM
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              /// 오운완, 페이지 기능 설명 위젯
               SelfCareTodayExerciseMainTitle(),
               SizedBox(height: 32),
+              /// 사진, 앨범 페이지 관리 container
               SelfCareTodayExercisePageContainer(),
             ],
           ),
         ),
       ),
+      /// Figma 디자인에 맞춘 Custom TabBar
       bottomNavigationBar: const SelfCareTodayExerciseTabBar(),
+      /// FAB도 보기 좋게 따로 나눴습니다.
       floatingActionButton: const SelfCareTodayExerciseFAB(),
+      /// FAB 위치 지정
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
