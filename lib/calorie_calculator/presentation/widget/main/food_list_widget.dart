ab8baa71fd78f99522b2fa5e3ae8b89f76edc8de
ljyo2o9 <yggosxoljy@gmail.com>
2024-01-10 11:10:59 +0900

    🗒️ :: [#145] foodList를 보여주는 FoodListWidget 생성

---
+++ lib/calorie_calculator/presentation/widget/main/food_list_widget.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../config/maeumgagym_color.dart';
+import '../../../../core/component/text/pretendard/ptd_text_widget.dart';
+import 'package:maeum_ga_gym_flutter/calorie_calculator/presentation/provider/food_list_provider.dart';
+
+class FoodListWidget extends ConsumerStatefulWidget {
+  const FoodListWidget({super.key});
+
+  @override
+  ConsumerState<FoodListWidget> createState() => _FoodListWidgetState();
+}
+
+class _FoodListWidgetState extends ConsumerState<FoodListWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final listController = ref.watch(foodListController);
+    final listControllerNotifier = ref.read(foodListController.notifier);
+
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: listController.length,
+      itemBuilder: (context, index) {
+        return SizedBox(
+          height: 72,
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    PtdTextWidget.labelLarge(
+                      '김밥',
+                      MaeumgagymColor.black,
+                    ),
+                    PtdTextWidget.bodyMedium(
+                      '중접시(1줄)',
+                      MaeumgagymColor.gray500,
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    PtdTextWidget.labelLarge(
+                      '0kal',
+                      MaeumgagymColor.black,
+                    ),
+                    const SizedBox(width: 16),
+                    GestureDetector(
+                      onTap: () {
+                        listControllerNotifier.delFoodList(index);
+                        setState(() {});
+                      },
+                      child: Image.asset(
+                        'assets/image/core_icon/close.png',
+                        width: 12,
+                        height: 12,
+                        color: MaeumgagymColor.gray200,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
