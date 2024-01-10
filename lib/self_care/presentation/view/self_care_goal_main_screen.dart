d888a8f5cd66fb16cdbbf62438255a59f6179579
iyes826 <treeyu12345@naver.com>
2024-01-11 00:27:01 +0900

    ⚓️ :: [#172] AppBar 적용

--- lib/self_care/presentation/view/self_care_goal_main_screen.dart
+++ lib/self_care/presentation/view/self_care_goal_main_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/self_care_default_app_bar.dart';
 
 class SelfCareGoalMainScreen extends StatelessWidget {
   const SelfCareGoalMainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: SelfCareDefaultAppBar(
+        iconPath: "assets/image/core_icon/left_arrow_icon.svg",
+      ),
+    );
   }
 }
