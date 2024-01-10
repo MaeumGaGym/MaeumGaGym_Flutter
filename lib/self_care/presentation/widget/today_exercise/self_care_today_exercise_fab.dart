145b100f1bd938afc9257734094b5b050c640dcf
iyes826 <treeyu12345@naver.com>
2024-01-10 21:17:14 +0900

    🗒️ :: [#159] SelfCareTodayExerciseFAB class 화

---
+++ lib/self_care/presentation/widget/today_exercise/self_care_today_exercise_fab.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
+
+class SelfCareTodayExerciseFAB extends StatelessWidget {
+  const SelfCareTodayExerciseFAB({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 64,
+      width: 64,
+      child: FloatingActionButton(
+        onPressed: () {},
+        shape: const CircleBorder(),
+        backgroundColor: MaeumgagymColor.blue500,
+        heroTag: "cameraButton",
+        child: SvgPicture.asset(
+          "assets/image/self_care_icon/camera_icon.svg",
+          color: MaeumgagymColor.white,
+          width: 40,
+          height: 40,
+        ),
+      ),
+    );
+  }
+}
