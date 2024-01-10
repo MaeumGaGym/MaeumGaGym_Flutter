e38a3b038fb3f61f5a810240f1b017a8b8bb6055
iyes826 <treeyu12345@naver.com>
2024-01-11 00:48:15 +0900

    🗒️ :: [#172] SelfCareGoalRoutineItemWidget 추가

---
+++ lib/self_care/presentation/widget/goal/widget/self_care_goal_routine_item_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SelfCareGoalRoutineItemWidget extends StatelessWidget {
+  const SelfCareGoalRoutineItemWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
