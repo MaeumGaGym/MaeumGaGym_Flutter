b8fb2f5823b3ea1de1b3f9d2c46065bb6a7c0027
iyes826 <treeyu12345@naver.com>
2024-01-10 17:20:34 +0900

    🗒️ :: [#159] SelfCareTodayExerciseTabBar 추가

---
+++ lib/self_care/presentation/widget/today_exercise/self_care_today_exercise_tab_bar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
+import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/provider/self_care_today_exercise_tab_bar_provider.dart';
+
+class SelfCareTodayExerciseTabBar extends ConsumerWidget {
+  const SelfCareTodayExerciseTabBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SizedBox(
+      height: 72,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          GestureDetector(
+            onTap: () {
+
+            },
+            child: Container(
+              width: 83,
+              height: 40,
+              decoration: BoxDecoration(
+                color: MaeumgagymColor.gray50,
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Center(
+                child: PtdTextWidget.labelLarge(
+                  "사진",
+                  MaeumgagymColor.blue500,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(width: 16),
+          Container(
+            width: 83,
+            height: 40,
+            decoration: BoxDecoration(
+              color: MaeumgagymColor.gray50,
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: Center(
+              child: PtdTextWidget.labelLarge(
+                "앨범",
+                MaeumgagymColor.blue500,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
