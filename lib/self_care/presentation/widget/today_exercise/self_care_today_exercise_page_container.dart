6c2e29bf802fce51b53ce95bdd7204e20728d997
iyes826 <treeyu12345@naver.com>
2024-01-10 21:27:45 +0900

    🗒️ :: [#159] SelfCareTodayExercisePageContainer 추가

---
+++ lib/self_care/presentation/widget/today_exercise/self_care_today_exercise_page_container.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/view/self_care_today_exercise_camera_screen.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/view/self_care_today_exercise_gallery_screen.dart';
+
+class SelfCareTodayExercisePageContainer extends StatefulWidget {
+  const SelfCareTodayExercisePageContainer({Key? key}) : super(key: key);
+
+  @override
+  State<SelfCareTodayExercisePageContainer> createState() => _SelfCareTodayExercisePageContainerState();
+}
+
+class _SelfCareTodayExercisePageContainerState extends State<SelfCareTodayExercisePageContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
