a660eaf9bc9030dfa8156e584aac0534643d5a9e
iyes826 <treeyu12345@naver.com>
2024-01-10 21:31:59 +0900

    💉 :: [#159] stful >> consumerstful

--- lib/self_care/presentation/widget/today_exercise/self_care_today_exercise_page_container.dart
+++ lib/self_care/presentation/widget/today_exercise/self_care_today_exercise_page_container.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/view/self_care_today_exercise_camera_screen.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/view/self_care_today_exercise_gallery_screen.dart';
 
-class SelfCareTodayExercisePageContainer extends StatefulWidget {
+class SelfCareTodayExercisePageContainer extends ConsumerStatefulWidget {
   const SelfCareTodayExercisePageContainer({Key? key}) : super(key: key);
 
   @override
-  State<SelfCareTodayExercisePageContainer> createState() => _SelfCareTodayExercisePageContainerState();
+  ConsumerState<SelfCareTodayExercisePageContainer> createState() =>
+      _SelfCareTodayExercisePageContainerState();
 }
 
-class _SelfCareTodayExercisePageContainerState extends State<SelfCareTodayExercisePageContainer> {
+class _SelfCareTodayExercisePageContainerState
+    extends ConsumerState<SelfCareTodayExercisePageContainer> {
+  late PageController pageController;
+
+  @override
+  void initState() {
+    pageController = PageController(initialPage: 0);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return PageView(
+      controller:,
+      children: [
+        SelfCareTodayExerciseCameraScreen(),
+        SelfCareTodayExerciseGalleryScreen(),
+      ],
+    );
   }
 }
