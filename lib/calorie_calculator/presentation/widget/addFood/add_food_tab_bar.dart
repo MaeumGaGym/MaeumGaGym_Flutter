3e3b765de786e6ea4805a674bf841371dd4a7f29
ljyo2o9 <yggosxoljy@gmail.com>
2024-01-11 22:44:21 +0900

    🗒️ :: [#185] AddFoodTabBar 생성

---
+++ lib/calorie_calculator/presentation/widget/addFood/add_food_tab_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddFoodTabBar extends StatelessWidget {
+  const AddFoodTabBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
