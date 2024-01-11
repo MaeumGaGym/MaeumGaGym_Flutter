8073bb5a63d4611f6b28437a4180aa0c36c76986
iyes826 <treeyu12345@naver.com>
2024-01-11 12:18:49 +0900

    ⚓️ :: [#177] SelfCareRoutineItemNotifier 세팅

---
+++ lib/self_care/presentation/provider/self_care_routine_item_provider.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/model/self_care_routine_item_model.dart';
+
+class SelfCareRoutineItemNotifier extends StateNotifier<List<SelfCareRoutineItemModel>> {
+  SelfCareRoutineItemNotifier() : super([]);
+}
