c4a504efc16208b8863bb7764fc8b56702946755
iyes826 <treeyu12345@naver.com>
2024-01-11 19:41:33 +0900

    ⚓️ :: [#179] isKept Provider 적용

--- lib/self_care/presentation/widget/goal/widget/self_care_goal_routine_manage_bottom_sheet.dart
+++ lib/self_care/presentation/widget/goal/widget/self_care_goal_routine_manage_bottom_sheet.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/provider/self_care_routine_item_provider.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/goal/widget/self_care_goal_routine_manage_item_widget.dart';
 
 class SelfCareGoalRoutineManageBottomSheet extends ConsumerWidget {
   final bool isShared;
   final bool isKept;
+  final int index;
 
   const SelfCareGoalRoutineManageBottomSheet({
     Key? key,
     required this.isShared,
     required this.isKept,
+    required this.index,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final routineItemNotifier = ref.watch(selfCareRoutineItemProvider.notifier);
     return SizedBox(
       width: MediaQuery.of(context).size.width,
       child: Column(
@@ -39,9 +43,18 @@ class SelfCareGoalRoutineManageBottomSheet extends ConsumerWidget {
                 title: "수정",
                 iconPath: "assets/image/self_care_icon/edit_pencil_icon.svg",
               ),
-              SelfCareGoalRoutineManageItemWidget(
-                title: isKept ? "보관 취소" : "보관",
-                iconPath: "assets/image/self_care_icon/inbox_icon.svg",
+              GestureDetector(
+                onTap: () {
+                  if (isKept == true) {
+                    routineItemNotifier.cancelKeepRoutineItem(index);
+                  } else {
+                    routineItemNotifier.keepRoutineItem(index);
+                  }
+                },
+                child: SelfCareGoalRoutineManageItemWidget(
+                  title: isKept ? "보관 취소" : "보관",
+                  iconPath: "assets/image/self_care_icon/inbox_icon.svg",
+                ),
               ),
               SelfCareGoalRoutineManageItemWidget(
                 title: isShared ? "공유 취소" : "공유",
