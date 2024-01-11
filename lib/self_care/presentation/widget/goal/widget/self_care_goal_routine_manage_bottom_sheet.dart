553b769528f29c3895c8bab865d8413646e300bc
iyes826 <treeyu12345@naver.com>
2024-01-11 11:28:30 +0900

    🗒️ :: [#175] SelfCareGoalRoutineManageBottomSheet 추가

---
+++ lib/self_care/presentation/widget/goal/widget/self_care_goal_routine_manage_bottom_sheet.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
+import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
+
+class SelfCareGoalRoutineManageBottomSheet extends StatelessWidget {
+  const SelfCareGoalRoutineManageBottomSheet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const SizedBox(height: 10),
+          Container(
+            width: 64,
+            height: 5,
+            decoration: BoxDecoration(
+              color: MaeumgagymColor.gray300,
+              borderRadius: BorderRadius.circular(2),
+            ),
+          ),
+          Column(
+            children: [
+
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
