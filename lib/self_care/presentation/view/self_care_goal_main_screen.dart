baa234ab951270a629a1180aedc7d678f09cd49c
iyes826 <treeyu12345@naver.com>
2024-01-11 13:35:58 +0900

    💉 :: [#177] stless >> consumerWidget

--- lib/self_care/presentation/view/self_care_goal_main_screen.dart
+++ lib/self_care/presentation/view/self_care_goal_main_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/goal/widget/self_care_goal_main_title.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/goal/widget/self_care_goal_routine_item_widget.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/self_care_default_app_bar.dart';
 
-class SelfCareGoalMainScreen extends StatelessWidget {
+class SelfCareGoalMainScreen extends ConsumerWidget {
   const SelfCareGoalMainScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: SelfCareDefaultAppBar(
         iconPath: "assets/image/core_icon/left_arrow_icon.svg",
