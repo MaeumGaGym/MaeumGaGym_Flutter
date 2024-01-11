993040d630e47a358f93c5bc6b8c3a083fd40b26
iyes826 <treeyu12345@naver.com>
2024-01-11 11:30:11 +0900

    🗒️ :: [#175] SelfCareGoalRoutineManageItemWidget 추가

---
+++ lib/self_care/presentation/widget/goal/widget/self_care_goal_routine_manage_item_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
+import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
+
+class SelfCareGoalRoutineManageItemWidget extends StatelessWidget {
+  const SelfCareGoalRoutineManageItemWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SvgPicture.asset(
+              "assets/image/self_care_icon/edit_pencil_icon.svg",
+              width: 28,
+              height: 28,
+            ),
+            const SizedBox(width: 24),
+            PtdTextWidget.labelLarge(
+              "수정",
+              MaeumgagymColor.black,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
