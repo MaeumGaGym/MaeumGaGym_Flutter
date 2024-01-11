f444fa285bc326bc49ea9025759df744ec1ff843
iyes826 <treeyu12345@naver.com>
2024-01-11 20:21:45 +0900

    ⚓️ :: [#181] itemIndex 추가

--- lib/self_care/presentation/model/self_care_routine_item_model.dart
+++ lib/self_care/presentation/model/self_care_routine_item_model.dart
@@ -1,4 +1,6 @@
 class SelfCareRoutineItemModel {
+  /// 아이템 번호
+  final int itemIndex;
   /// 제목
   final String title;
   /// 보관중인지 >> true = 보관중 , false = 사용중
@@ -7,6 +9,7 @@ class SelfCareRoutineItemModel {
   bool isShared;
 
   SelfCareRoutineItemModel({
+    required this.itemIndex,
     required this.title,
     this.isKept = true,
     this.isShared = false,
