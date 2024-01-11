7190363055916c43d19a775bb1c8730b5d93115c
iyes826 <treeyu12345@naver.com>
2024-01-11 12:31:11 +0900

    ⚓️ :: [#177] selfCareRoutineItemProvider 추가

--- lib/self_care/presentation/provider/self_care_routine_item_provider.dart
+++ lib/self_care/presentation/provider/self_care_routine_item_provider.dart
@@ -1,13 +1,19 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/model/self_care_routine_item_model.dart';
 
+final selfCareRoutineItemProvider = StateNotifierProvider<SelfCareRoutineItemNotifier, List<SelfCareRoutineItemModel>>((ref) {
+  return SelfCareRoutineItemNotifier();
+});
+
 class SelfCareRoutineItemNotifier extends StateNotifier<List<SelfCareRoutineItemModel>> {
   SelfCareRoutineItemNotifier() : super([]);
 
   /// 나중에 기능 구현하겠습니다.
   void addRoutineItem() {}
 
-  void keepRoutineItem() {}
+  void keepRoutineItem() {
+
+  }
 
   void cancelKeepRoutineItem() {}
 
