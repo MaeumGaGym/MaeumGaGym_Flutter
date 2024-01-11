07270ae9ec870d036365473387adf8eec2c4beb5
ljyo2o9 <yggosxoljy@gmail.com>
2024-01-11 21:09:04 +0900

    🗒️ :: [#183] TabContents에서 TabBar 수정

--- lib/pose/presentation/widget/tab/tab_contents.dart
+++ lib/pose/presentation/widget/tab/tab_contents.dart
@@ -1,12 +1,15 @@
 /// Package
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
+import 'package:maeum_ga_gym_flutter/pose/presentation/widget/tab/tab_contants_tab_bar.dart';
 
 /// Screen
 import '../../view/pose_part_screen.dart';
 import '../../view/pose_recommend_screen.dart';
 
 class TabContents extends StatelessWidget {
-  final Widget tabBar;
+  final List<String> tabBar;
   final TabController tabController;
 
   const TabContents(
@@ -17,43 +20,23 @@ class TabContents extends StatelessWidget {
     return Column(
       children: [
         /// Tap Bar
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8),
-          child: SizedBox(height: 56, child: tabBar),
-        ),
-
-        /// Tab Contents
-        Expanded(
-          child: TabBarView(
-            controller: tabController,
-            children: const [
-              PoseReCommendScreen(),
-              PosePartScreen(
-                tabName: ['상체', '가슴'],
+        Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 54),
+              child: Container(
+                height: 2,
+                color: MaeumgagymColor.gray50,
               ),
-              PosePartScreen(
-                tabName: ['상체', '등'],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: SizedBox(
+                height: 56,
+                child: TabContentsTabBar(tabData: tabBar),
               ),
-              PosePartScreen(
-                tabName: ['상체', '어깨'],
-              ),
-              PosePartScreen(
-                tabName: ['상체', '팔'],
-              ),
-              PosePartScreen(
-                tabName: ['상체', '복근'],
-              ),
-              PosePartScreen(
-                tabName: ['하체', '앞 허벅지'],
-              ),
-              PosePartScreen(
-                tabName: ['하체', '뒷 허벅지'],
-              ),
-              PosePartScreen(
-                tabName: ['하체', '종아리'],
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ],
     );
