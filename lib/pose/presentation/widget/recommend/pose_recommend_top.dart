0006278a7efffd905458d0f928758172d9c12cf1
ljyo2o9 <yggosxoljy@gmail.com>
2024-01-11 23:47:09 +0900

    🗒️ :: [#189] PoseRecommendTop 생성

---
+++ lib/pose/presentation/widget/recommend/pose_recommend_top.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PoseRecommendTop extends StatelessWidget {
+  const PoseRecommendTop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
