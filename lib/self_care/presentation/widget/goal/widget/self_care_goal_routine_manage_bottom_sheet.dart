73dc6dcf068c32616496ed8da1cbc216527c27f9
iyes826 <treeyu12345@naver.com>
2024-01-11 11:50:33 +0900

    ⚓️ :: [#175] SelfCareGoalRoutineManageItemWidget 적용

--- lib/self_care/presentation/widget/goal/widget/self_care_goal_routine_manage_bottom_sheet.dart
+++ lib/self_care/presentation/widget/goal/widget/self_care_goal_routine_manage_bottom_sheet.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
-import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/goal/widget/self_care_goal_routine_manage_item_widget.dart';
 
 class SelfCareGoalRoutineManageBottomSheet extends StatelessWidget {
   const SelfCareGoalRoutineManageBottomSheet({Key? key}) : super(key: key);
@@ -24,8 +23,24 @@ class SelfCareGoalRoutineManageBottomSheet extends StatelessWidget {
             ),
           ),
           Column(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-
+              SelfCareGoalRoutineManageItemWidget(
+                title: "수정",
+                iconPath: "assets/image/self_care_icon/edit_pencil_icon.svg",
+              ),
+              SelfCareGoalRoutineManageItemWidget(
+                title: "보관",
+                iconPath: "assets/image/self_care_icon/inbox_icon.svg",
+              ),
+              SelfCareGoalRoutineManageItemWidget(
+                title: "공유",
+                iconPath: "assets/image/self_care_icon/earth_icon.svg",
+              ),
+              SelfCareGoalRoutineManageItemWidget(
+                title: "삭제",
+                iconPath: "assets/image/self_care_icon/edit_trash_icon.svg",
+              ),
             ],
           ),
         ],
