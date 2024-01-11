3c9ef3b64dd8bca1ab9c1df733907349046c18e5
ljyo2o9 <yggosxoljy@gmail.com>
2024-01-11 22:49:20 +0900

    🗒️ :: [#185] AddFoodTabBar 주석처리

--- lib/calorie_calculator/presentation/widget/addFood/add_food_tab_bar.dart
+++ lib/calorie_calculator/presentation/widget/addFood/add_food_tab_bar.dart
@@ -1,10 +1,13 @@
-import 'package:flutter/cupertino.dart';
+/// Package
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
-import 'package:maeum_ga_gym_flutter/calorie_calculator/presentation/provider/food_tab_bar_controller.dart';
 
+/// Core
+import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
 import '../../../../core/component/text/pretendard/ptd_text_widget.dart';
+
+/// Provider
+import 'package:maeum_ga_gym_flutter/calorie_calculator/presentation/provider/food_tab_bar_controller.dart';
 
 class AddFoodTabBar extends ConsumerWidget {
   const AddFoodTabBar({super.key});
@@ -14,6 +17,7 @@ class AddFoodTabBar extends ConsumerWidget {
     final tabController = ref.watch(foodTabController);
     final tabControllerNotifier = ref.read(foodTabController.notifier);
 
+    /// Tab에 들어갈 데이터
     List<String> tabData = [
       "MY",
       "전체",
@@ -25,6 +29,7 @@ class AddFoodTabBar extends ConsumerWidget {
       "패스트푸드",
     ];
 
+    /// Tab의 Border를 관리하는 함수
     BorderSide borderSide(bool state) {
       if (state) {
         return BorderSide(width: 2, color: MaeumgagymColor.gray50);
@@ -33,6 +38,7 @@ class AddFoodTabBar extends ConsumerWidget {
       }
     }
 
+    /// 양엽 Tab Padding
     Padding tabPadding() {
       return Padding(
         padding: const EdgeInsets.only(top: 42),
@@ -48,9 +54,12 @@ class AddFoodTabBar extends ConsumerWidget {
       scrollDirection: Axis.horizontal,
       child: Row(
         children: [
+          /// Tab Padding
           tabPadding(),
           SizedBox(
             height: 44,
+
+            /// Tab을 구현하기 위한 ListView.builder
             child: ListView.builder(
               shrinkWrap: true,
               physics: const NeverScrollableScrollPhysics(),
@@ -59,6 +68,7 @@ class AddFoodTabBar extends ConsumerWidget {
               itemBuilder: (context, index) {
                 return Padding(
                   padding: EdgeInsets.only(
+                    /// BorderSide가 none이 되면 2만큼 주변 크기가 줄어듬 그것을 방지
                     top: tabController.index == index ? 0 : 2,
                   ),
                   child: GestureDetector(
@@ -67,6 +77,7 @@ class AddFoodTabBar extends ConsumerWidget {
                       alignment: Alignment.center,
                       decoration: BoxDecoration(
                         border: Border(
+                          /// bottom을 제외한 나머지는 Tab이 선택되면 생김
                           top: borderSide(tabController.index == index),
                           left: borderSide(tabController.index == index),
                           right: borderSide(tabController.index == index),
@@ -80,6 +91,7 @@ class AddFoodTabBar extends ConsumerWidget {
                         alignment: Alignment.center,
                         decoration: BoxDecoration(
                           border: Border(
+                            /// bottom Boder가 Tab이 선택되면 사라짐
                             bottom: borderSide(
                               !(tabController.index == index),
                             ),
@@ -88,6 +100,8 @@ class AddFoodTabBar extends ConsumerWidget {
                         child: Padding(
                           padding: EdgeInsets.symmetric(
                             vertical: 12,
+
+                            /// BorderSide가 white가 되면 디자인과 맞지 않아 bottom BorderSide를 줄려 디자인과 맞춤
                             horizontal: tabController.index == index ? 14 : 16,
                           ),
                           child: PtdTextWidget.bodyMedium(
