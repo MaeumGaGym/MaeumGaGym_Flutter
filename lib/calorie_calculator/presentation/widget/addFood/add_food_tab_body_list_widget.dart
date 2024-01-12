c87b5f8408646302ddc430727cca3054ceaf21a9
ljyo2o9 <yggosxoljy@gmail.com>
2024-01-12 15:34:08 +0900

    🗒️ :: [#191] AddFoodTabBodyListWidget 생성

---
+++ lib/calorie_calculator/presentation/widget/addFood/add_food_tab_body_list_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddFoodTabBodyListWidget extends StatelessWidget {
+  const AddFoodTabBodyListWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
