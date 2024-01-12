e7a02320dcbf1f971f025636eab898008bf13e2f
iyes826 <treeyu12345@naver.com>
2024-01-12 21:04:05 +0900

    ➕ :: [#205] 파라미터 추가

--- lib/self_care/presentation/widget/my_routine/widget/self_care_my_routine_button.dart
+++ lib/self_care/presentation/widget/my_routine/widget/self_care_my_routine_button.dart
@@ -4,8 +4,27 @@ import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
 import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
 
 class SelfCareMyRoutineButton extends StatelessWidget {
+  final double width;
+  final double height;
+  final double imageWidth;
+  final double imageHeight;
+  final String imagePath;
+  final String title;
+  final Color buttonColor;
+  final Color imageColor;
+  final Color textColor;
+
   const SelfCareMyRoutineButton({
     super.key,
+    required this.width,
+    required this.height,
+    required this.imageWidth,
+    required this.imageHeight,
+    required this.imagePath,
+    required this.title,
+    required this.buttonColor,
+    required this.imageColor,
+    required this.textColor,
   });
 
   @override
@@ -15,25 +34,25 @@ class SelfCareMyRoutineButton extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: 58,
+          width: width,
+          height: height,
           decoration: BoxDecoration(
-            color: MaeumgagymColor.blue500,
+            color: buttonColor,
             borderRadius: BorderRadius.circular(8),
           ),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               SvgPicture.asset(
-                "assets/image/self_care_icon/add_icon.svg",
-                width: 24,
-                height: 24,
-                color: MaeumgagymColor.white,
+                imagePath,
+                width: imageHeight,
+                height: imageWidth,
+                color: imageColor,
               ),
               const SizedBox(width: 8),
               PtdTextWidget.labelLarge(
-                "루틴 추가하기",
-                MaeumgagymColor.white,
+                title,
+                textColor,
               ),
             ],
           ),
