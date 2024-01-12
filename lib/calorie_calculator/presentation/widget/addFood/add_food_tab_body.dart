2f8256c2c793572efa9084fb7d73fa3ccde10074
ljyo2o9 <yggosxoljy@gmail.com>
2024-01-12 16:10:43 +0900

    🗒️ :: [#197] AddFoodTabBody 주석처리

--- lib/calorie_calculator/presentation/widget/addFood/add_food_tab_body.dart
+++ lib/calorie_calculator/presentation/widget/addFood/add_food_tab_body.dart
@@ -1,7 +1,11 @@
-import 'package:flutter/cupertino.dart';
+/// Package
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// Provider
 import 'package:maeum_ga_gym_flutter/calorie_calculator/presentation/provider/food_data_provider.dart';
+
+/// Widget
 import 'package:maeum_ga_gym_flutter/calorie_calculator/presentation/widget/addFood/add_food_tab_body_list_widget.dart';
 
 class AddFoodTabBody extends ConsumerWidget {
@@ -23,9 +27,11 @@ class AddFoodTabBody extends ConsumerWidget {
           final List<String> category =
               foodDataState.foodData[index]['category'];
 
+          /// Category에 tabName이 없다면 SizedBox.shrink()
           if (!(category.contains(tabName))) {
             return const SizedBox.shrink();
           } else {
+            /// AddFoodTabBodyListWidget
             return AddFoodTabBodyListWidget(
               index: index,
               g: foodDataState.foodData[index]['g'],
