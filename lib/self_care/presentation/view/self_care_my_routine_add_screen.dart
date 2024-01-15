7c9d8827a4f3da32211f315cf30734501815b7a2
iyes826 <treeyu12345@naver.com>
2024-01-15 20:53:43 +0900

    🗒️ :: [#207] SelfCareMyRoutineAddScreen 추가

---
+++ lib/self_care/presentation/view/self_care_my_routine_add_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SelfCareMyRoutineAddScreen extends StatelessWidget {
+  const SelfCareMyRoutineAddScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
