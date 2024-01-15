ba21648dc06f290707185d8edd0f3f843b72e774
iyes826 <treeyu12345@naver.com>
2024-01-15 21:47:18 +0900

    ➕ :: [#207] AppBar 적용

--- lib/self_care/presentation/view/self_care_my_routine_add_screen.dart
+++ lib/self_care/presentation/view/self_care_my_routine_add_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/self_care_default_app_bar.dart';
 
 class SelfCareMyRoutineAddScreen extends StatelessWidget {
   const SelfCareMyRoutineAddScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: SelfCareDefaultAppBar(
+        iconPath: "assets/image/core_icon/left_arrow_icon.svg",
+        title: "루틴 추가",
+      ),
+    );
   }
 }
