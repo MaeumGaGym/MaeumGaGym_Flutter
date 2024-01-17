6a75c006b9af17ac33097887bc17b2f55ee9e9f5
iyes826 <treeyu12345@naver.com>
2024-01-17 20:01:39 +0900

    💉 :: [#207] 버튼 수정

--- lib/self_care/presentation/widget/my_routine/widget/self_care_my_routine_button.dart
+++ lib/self_care/presentation/widget/my_routine/widget/self_care_my_routine_button.dart
@@ -1,59 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
 import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
 
 class SelfCareMyRoutineButton extends StatelessWidget {
   final double width;
   final double height;
-  final double imageWidth;
-  final double imageHeight;
-  final String imagePath;
   final String title;
   final Color buttonColor;
-  final Color imageColor;
   final Color textColor;
+
+  final double? imageWidth;
+  final double? imageHeight;
+  final String? imagePath;
+  final Color? imageColor;
 
   const SelfCareMyRoutineButton({
     super.key,
     required this.width,
     required this.height,
-    required this.imageWidth,
-    required this.imageHeight,
-    required this.imagePath,
     required this.title,
     required this.buttonColor,
-    required this.imageColor,
     required this.textColor,
+    this.imageWidth,
+    this.imageHeight,
+    this.imagePath,
+    this.imageColor,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Container(
-        width: width,
-        height: height,
-        decoration: BoxDecoration(
-          color: buttonColor,
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: buttonColor,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          /// image를 쓰는지 안 쓰는지 체크
+          if (imagePath != null)
             SvgPicture.asset(
-              imagePath,
+              imagePath!,
               width: imageHeight,
               height: imageWidth,
               color: imageColor,
             ),
-            const SizedBox(width: 8),
-            PtdTextWidget.labelLarge(
-              title,
-              textColor,
-            ),
-          ],
-        ),
+          /// 이미지를 안쓰면 간격 x
+          SizedBox(width: imagePath != null ? 8 : 0),
+          PtdTextWidget.labelLarge(
+            title,
+            textColor,
+          ),
+        ],
       ),
     );
   }
