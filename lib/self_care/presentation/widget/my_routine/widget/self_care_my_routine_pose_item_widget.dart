7031c8f768e0a9e6435b1868727b5939d6d078d6
iyes826 <treeyu12345@naver.com>
2024-01-19 20:10:49 +0900

    🗒️ :: [#215] SelfCareMyRoutinePoseItemWidget 추가

--- lib/self_care/presentation/widget/my_routine/widget/self_care_my_routine_pose_item_widget.dart
+++ lib/self_care/presentation/widget/my_routine/widget/self_care_my_routine_pose_item_widget.dart
@@ -2,9 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
 import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/my_routine/widget/self_care_my_routine_item_count_widget.dart';
 
-class SelfCareMyRoutinePoseItemWidget extends StatelessWidget {
-  const SelfCareMyRoutinePoseItemWidget({Key? key}) : super(key: key);
+class SelfCareMyRoutinePoseItemWidget extends StatefulWidget {
+  // final String title; // 나중에 기능 추가
+  // final String imagePath;
+
+  const SelfCareMyRoutinePoseItemWidget({
+    Key? key,
+    // required this.title,
+    // required this.imagePath,
+  }) : super(key: key);
+
+  @override
+  State<SelfCareMyRoutinePoseItemWidget> createState() =>
+      _SelfCareMyRoutinePoseItemWidgetState();
+}
+
+class _SelfCareMyRoutinePoseItemWidgetState
+    extends State<SelfCareMyRoutinePoseItemWidget> {
+  late TextEditingController countController;
+  late TextEditingController setController;
+
+  @override
+  void initState() {
+    super.initState();
+    /// 기본값은 1
+    countController = TextEditingController(text: "1");
+    setController = TextEditingController(text: "1");
+  }
+
+  @override
+  void dispose() {
+    countController.dispose();
+    setController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +48,7 @@ class SelfCareMyRoutinePoseItemWidget extends StatelessWidget {
           children: [
             Row(
               children: [
+                /// 아이템 사진
                 Container(
                   width: 80,
                   height: 80,
@@ -24,46 +58,29 @@ class SelfCareMyRoutinePoseItemWidget extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(width: 18),
+                /// 아이템 이름
                 PtdTextWidget.bodyLarge(
                   "푸시업",
                   MaeumgagymColor.black,
                 ),
               ],
             ),
-            SvgPicture.asset(
-              "assets/image/self_care_icon/close_icon.svg",
+            GestureDetector(
+              child: SvgPicture.asset(
+                "assets/image/self_care_icon/close_icon.svg",
+              ),
             ),
           ],
         ),
         const SizedBox(height: 12),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 18),
-              child: PtdTextWidget.bodyMedium(
-                "횟수",
-                MaeumgagymColor.black,
-              ),
-            ),
-            /// 횟수 입력 칸
-            const SizedBox.shrink(),
-          ],
+        SelfCareMyRoutineItemCountWidget(
+          title: "횟수",
+          controller: countController,
         ),
         const SizedBox(height: 12),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 18),
-              child: PtdTextWidget.bodyMedium(
-                "세트",
-                MaeumgagymColor.black,
-              ),
-            ),
-            /// 세트 수 입력 칸
-            const SizedBox.shrink(),
-          ],
+        SelfCareMyRoutineItemCountWidget(
+          title: "세트",
+          controller: setController,
         ),
       ],
     );
