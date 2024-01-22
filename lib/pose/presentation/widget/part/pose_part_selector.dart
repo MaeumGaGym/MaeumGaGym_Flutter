2c95b8a15e121334f0a5d2e9fa1504ce6c7dbe85
ljyo2o9 <yggosxoljy@gmail.com>
2024-01-22 15:35:24 +0900

    🗒️ :: [#222 ] PosePartSelectorWidget을 사용한 PosePartSelector 추가

--- lib/pose/presentation/widget/part/pose_part_selector.dart
+++ lib/pose/presentation/widget/part/pose_part_selector.dart
@@ -1,33 +1,44 @@
-/// Packge
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maeum_ga_gym_flutter/pose/presentation/widget/part/pose_part_selector_widget.dart';
 
-/// Core
-import '../../../../config/maeumgagym_color.dart';
-import '../../../../core/component/text/pretendard/ptd_text_widget.dart';
+import '../../provider/pose_part_controller.dart';
 
-class PosePartSelector extends StatelessWidget {
-  final bool state;
-
-  final String title;
-
-  const PosePartSelector({required this.state, required this.title, super.key});
+class PosePartSelector extends ConsumerWidget {
+  const PosePartSelector({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      /// Tag가 선택시 blue500 아닐 시 gray50
-      decoration: BoxDecoration(
-        color: state ? MaeumgagymColor.blue500 : MaeumgagymColor.gray50,
-        borderRadius: BorderRadius.circular(100),
-      ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final posePartState = ref.watch(posePartController);
+    final posePartNotifier = ref.read(posePartController.notifier);
 
-      /// Tag이름
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        child: PtdTextWidget.labelMedium(
-          title,
-          state ? MaeumgagymColor.white : MaeumgagymColor.gray600,
-        ),
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          /// 맨몸
+          GestureDetector(
+            onTap: () => posePartNotifier.clickCalisthenics(),
+            child: PosePartSelectorWidget(
+              title: '맨몸',
+              state: posePartState.calisthenics,
+            ),
+          ),
+
+          /// SizedBox
+          const SizedBox(width: 8),
+
+          /// 기구
+          GestureDetector(
+            onTap: () => posePartNotifier.clickMachine(),
+            child: PosePartSelectorWidget(
+              title: '기구',
+              state: posePartState.machine,
+            ),
+          ),
+        ],
       ),
     );
   }
