c2df98c0f434cb241179e80fb35988691e248477
유지우 <treeyu12345@naver.com>
2024-01-23 13:47:56 +0900

    💉 :: [#227] color 값 추가 + Container 보더 설정

--- lib/self_care/presentation/widget/my_routine/widget/self_care_my_routine_shared_widget.dart
+++ lib/self_care/presentation/widget/my_routine/widget/self_care_my_routine_shared_widget.dart
@@ -4,26 +4,37 @@ import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
 import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
 
 class SelfCareMyRoutineSharedWidget extends StatelessWidget {
-  const SelfCareMyRoutineSharedWidget({Key? key}) : super(key: key);
+  final Color? color;
+
+  const SelfCareMyRoutineSharedWidget({
+    Key? key,
+    this.color,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: Row(
-        children: [
-          PtdTextWidget.labelMedium(
-            "공유됨",
-            MaeumgagymColor.blue500,
-          ),
-          const SizedBox(width: 8),
-          SvgPicture.asset(
-            "assets/image/self_care_icon/earth_icon.svg",
-            width: 16,
-            height: 16,
-            color: MaeumgagymColor.blue500,
-          ),
-        ],
+    return Container(
+      decoration: BoxDecoration(
+        color: color ?? MaeumgagymColor.white,
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Row(
+          children: [
+            PtdTextWidget.labelMedium(
+              "공유됨",
+              MaeumgagymColor.blue500,
+            ),
+            const SizedBox(width: 8),
+            SvgPicture.asset(
+              "assets/image/self_care_icon/earth_icon.svg",
+              width: 16,
+              height: 16,
+              color: MaeumgagymColor.blue500,
+            ),
+          ],
+        ),
       ),
     );
   }
