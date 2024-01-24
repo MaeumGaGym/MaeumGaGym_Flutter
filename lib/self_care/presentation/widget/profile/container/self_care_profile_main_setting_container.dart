e0f341facb063fc07918625f42962ee63ad2e1b4
유지우 <treeyu12345@naver.com>
2024-01-24 15:14:06 +0900

    🏃🏿‍♂️ :: [#235] Profile 디렉토리 생성 >> 파일 구조 변경

--- lib/self_care/presentation/widget/profile/container/self_care_profile_main_setting_container.dart
+++ lib/self_care/presentation/widget/profile/container/self_care_profile_main_setting_container.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
 import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
-import 'package:maeum_ga_gym_flutter/self_care/presentation/view/self_care_profile_edit_screen.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/view/profile/self_care_profile_edit_screen.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/profile/widget/dialog/self_care_profile_quit_dialog.dart';
 
 class SelfCareProfileMainSettingContainer extends StatelessWidget {
