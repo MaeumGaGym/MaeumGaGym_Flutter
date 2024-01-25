ff4ea1a9b96c79c4049ee502f17984f0a9726022
유지우 <treeyu12345@naver.com>
2024-01-25 14:27:18 +0900

    🗒️ :: [#243] SelfCareGoalDetailScreen 추가

---
+++ lib/self_care/presentation/view/goal/self_care_goal_detail_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SelfCareGoalDetailScreen extends StatelessWidget {
+  const SelfCareGoalDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
