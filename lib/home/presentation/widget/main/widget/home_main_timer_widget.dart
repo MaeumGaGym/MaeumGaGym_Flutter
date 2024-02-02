ffdf10193adaff2c39e07a8b8587ea7d3aa0a334
유지우 <treeyu12345@naver.com>
2024-02-02 15:00:11 +0900

    💉 :: [#255] Navigator 설정

--- lib/home/presentation/widget/main/widget/home_main_timer_widget.dart
+++ lib/home/presentation/widget/main/widget/home_main_timer_widget.dart
@@ -4,6 +4,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
 import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
 import 'package:maeum_ga_gym_flutter/home/presentation/providers/timer_state_provider.dart';
+import 'package:maeum_ga_gym_flutter/home/presentation/view/home_timer_screen.dart';
 
 class HomeMainTimerWidget extends ConsumerStatefulWidget {
   const HomeMainTimerWidget({Key? key}) : super(key: key);
@@ -30,54 +31,80 @@ class _MainTimerWidgetState extends ConsumerState<HomeMainTimerWidget> {
                   Padding(
                     padding:
                         const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            SvgPicture.asset(
-                              "assets/image/home_icon/timer_icon.svg",
-                              color: MaeumgagymColor.gray400,
-                              width: 24,
-                              height: 24,
-                            ),
-                            const SizedBox(width: 24),
-                            PtdTextWidget.titleLarge(
-                              Duration(seconds: timerList[index].currentTime.toInt()).toString().split(".").first.substring(2, 7),
-                              timerList[index].timerState == TimerState.started ? MaeumgagymColor.blue500 : MaeumgagymColor.gray400,
-                            ),
-                          ],
+                    child: GestureDetector(
+                      behavior: HitTestBehavior.translucent,
+                      onTap: () => Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => const HomeTimerScreen(),
                         ),
-                        GestureDetector(
-                          onTap: () {
-                            if (timerList[index].timerState == TimerState.paused || timerList[index].timerState == TimerState.initial) {
-                              ref.read(timersProvider.notifier).onStarted(timerList[index].timerId);
-                            } else {
-                              ref.read(timersProvider.notifier).onPaused(timerList[index].timerId);
-                            }
-                          },
-                          child: Container(
-                            width: 40,
-                            height: 40,
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              color: timerList[index].timerState ==
-                                      TimerState.started
-                                  ? MaeumgagymColor.gray50
-                                  : MaeumgagymColor.blue500,
-                              shape: BoxShape.circle,
-                            ),
-                            child: SvgPicture.asset(
-                              timerList[index].timerState == TimerState.started
-                                  ? 'assets/image/home_icon/pause_icon.svg'
-                                  : 'assets/image/home_icon/play_filled_icon.svg',
-                              color: MaeumgagymColor.white,
-                              width: 20,
-                              height: 20,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              SvgPicture.asset(
+                                "assets/image/home_icon/timer_icon.svg",
+                                color: MaeumgagymColor.gray400,
+                                width: 24,
+                                height: 24,
+                              ),
+                              const SizedBox(width: 24),
+                              PtdTextWidget.titleLarge(
+                                Duration(
+                                        seconds: timerList[index]
+                                            .currentTime
+                                            .toInt())
+                                    .toString()
+                                    .split(".")
+                                    .first
+                                    .substring(2, 7),
+                                timerList[index].timerState ==
+                                        TimerState.started
+                                    ? MaeumgagymColor.blue500
+                                    : MaeumgagymColor.gray400,
+                              ),
+                            ],
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              if (timerList[index].timerState ==
+                                      TimerState.paused ||
+                                  timerList[index].timerState ==
+                                      TimerState.initial) {
+                                ref
+                                    .read(timersProvider.notifier)
+                                    .onStarted(timerList[index].timerId);
+                              } else {
+                                ref
+                                    .read(timersProvider.notifier)
+                                    .onPaused(timerList[index].timerId);
+                              }
+                            },
+                            child: Container(
+                              width: 40,
+                              height: 40,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                color: timerList[index].timerState ==
+                                        TimerState.started
+                                    ? MaeumgagymColor.gray50
+                                    : MaeumgagymColor.blue500,
+                                shape: BoxShape.circle,
+                              ),
+                              child: SvgPicture.asset(
+                                timerList[index].timerState ==
+                                        TimerState.started
+                                    ? 'assets/image/home_icon/pause_icon.svg'
+                                    : 'assets/image/home_icon/play_filled_icon.svg',
+                                color: MaeumgagymColor.white,
+                                width: 20,
+                                height: 20,
+                              ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                   SizedBox(height: index == timerList.length - 1 ? 0 : 18),
