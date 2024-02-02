f45aef0f6326a7a9bb916eee0f1f1ff9a018428d
유지우 <treeyu12345@naver.com>
2024-02-02 14:59:51 +0900

    🗒️ :: [#255] HomeTimerScreen 추가

---
+++ lib/home/presentation/view/home_timer_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeTimerScreen extends StatefulWidget {
+  const HomeTimerScreen({super.key});
+
+  @override
+  State<HomeTimerScreen> createState() => _HomeTimerScreenState();
+}
+
+class _HomeTimerScreenState extends State<HomeTimerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
