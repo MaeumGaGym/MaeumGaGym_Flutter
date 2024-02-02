a2c28881bc707927f6fdcd6102f6dd0eee97b5a0
유지우 <treeyu12345@naver.com>
2024-02-02 15:23:05 +0900

    🗒️ :: [#255] HomeTimerCircularTimerContainer 추가

---
+++ lib/home/presentation/widget/timer/container/home_timer_circular_timer_container.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeTimerCircularTimerContainer extends StatefulWidget {
+  const HomeTimerCircularTimerContainer({super.key});
+
+  @override
+  State<HomeTimerCircularTimerContainer> createState() => _HomeTimerCircularTimerContainerState();
+}
+
+class _HomeTimerCircularTimerContainerState extends State<HomeTimerCircularTimerContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
