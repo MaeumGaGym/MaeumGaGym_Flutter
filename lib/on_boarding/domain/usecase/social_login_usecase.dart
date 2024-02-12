89e65ebb54435aa5b0c51b7252e5df6b08fb2e1c
ljyo2o9 <yggosxoljy@gmail.com>
2024-02-12 22:47:38 +0900

    ⚓️ :: [#256] social_login_usecase 완성

---
+++ lib/on_boarding/domain/usecase/social_login_usecase.dart
@@ -0,0 +1,19 @@
+import 'package:maeum_ga_gym_flutter/on_boarding/domain/repository/social_login_repository.dart';
+
+class SocialLoginUseCase {
+  final SocialLoginRepository _repository;
+
+  SocialLoginUseCase(this._repository);
+
+  Future<bool> login() {
+    return _repository.login();
+  }
+
+  Future<bool> logout() {
+    return _repository.logout();
+  }
+
+  Future<String> getToken() {
+    return _repository.getToken();
+  }
+}
