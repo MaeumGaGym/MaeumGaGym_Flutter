567c987c0da724b5fb166d6f7ead1a9328bd0b65
유지우 <treeyu12345@naver.com>
2024-02-14 17:16:58 +0900

    🗒️ :: [#255] HomeTimerCircularTimerContainer 추가

--- lib/home/presentation/widget/timer/container/home_timer_circular_timer_container.dart
+++ lib/home/presentation/widget/timer/container/home_timer_circular_timer_container.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
+import 'package:maeum_ga_gym_flutter/home/presentation/providers/timer_state_provider.dart';
+import 'package:maeum_ga_gym_flutter/home/presentation/widget/timer/widget/home_timer_widget.dart';
 
-class HomeTimerCircularTimerContainer extends StatefulWidget {
+class HomeTimerCircularTimerContainer extends ConsumerStatefulWidget {
   const HomeTimerCircularTimerContainer({super.key});
 
   @override
-  State<HomeTimerCircularTimerContainer> createState() => _HomeTimerCircularTimerContainerState();
+  ConsumerState<HomeTimerCircularTimerContainer> createState() =>
+      _HomeTimerCircularTimerContainerState();
 }
 
-class _HomeTimerCircularTimerContainerState extends State<HomeTimerCircularTimerContainer> with TickerProviderStateMixin{
+class _HomeTimerCircularTimerContainerState extends ConsumerState<HomeTimerCircularTimerContainer> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    final timerState = ref.watch(timersProvider);
+    final timerIndex = ref.watch(selectedTimerProvider);
+    final timerController = ref.watch(homeTimerProvider);
+    return HomeTimerWidget(
+      controller: timerController,
+      duration: timerState[timerIndex].currentTime.inSeconds,
+      width: MediaQuery.of(context).size.width - 80,
+      height: MediaQuery.of(context).size.width - 80,
+      fillColor: MaeumgagymColor.blue400,
+      autoStart: false,
+      isReverse: true,
+      isReverseAnimation: true,
+      ringColor: MaeumgagymColor.gray100,
+      strokeWidth: 6,
+      strokeCap: StrokeCap.round,
+      onStart: () {
+        timerController.start();
+      },
+    );
   }
 }
