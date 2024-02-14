84e034dac2efdf0564be07d1333611c8e8204537
유지우 <treeyu12345@naver.com>
2024-02-14 17:14:40 +0900

    🗒️ :: [#255] HomeTimerFunctionWidget 추가
    리셋, 재생, 정지 등의 기능 버튼들을 관리하기 위한 위젯입니다.

---
+++ lib/home/presentation/widget/timer/widget/home_timer_function_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeTimerFunctionWidget extends StatelessWidget {
+  const HomeTimerFunctionWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
