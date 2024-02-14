a16345ee0d5aa7d60c786b6b019d76f18e0ef139
유지우 <treeyu12345@naver.com>
2024-02-14 17:16:36 +0900

    ⚓️ :: [#255] 재생 + 정지 버튼 기능 추가

--- lib/home/presentation/widget/timer/container/home_timer_func_button_list_container.dart
+++ lib/home/presentation/widget/timer/container/home_timer_func_button_list_container.dart
@@ -1,13 +1,19 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
+import 'package:maeum_ga_gym_flutter/home/presentation/providers/timer_state_provider.dart';
 
 class HomeTimerFuncButtonListContainer extends ConsumerWidget {
   const HomeTimerFuncButtonListContainer({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final timerController = ref.watch(homeTimerProvider);
+    final timerIndex = ref.watch(selectedTimerProvider);
+    final timerState = ref.watch(timersProvider);
+    final timerNotifier = ref.read(timersProvider.notifier);
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -31,7 +37,13 @@ class HomeTimerFuncButtonListContainer extends ConsumerWidget {
           padding: const EdgeInsets.symmetric(horizontal: 24),
           child: GestureDetector(
             onTap: () {
-
+              if (timerState[timerIndex].timerState != TimerState.started) {
+                timerNotifier.onStarted(timerIndex + 1);
+                timerController.resume();
+              } else {
+                timerNotifier.onPaused(timerIndex + 1);
+                timerController.pause();
+              }
             },
             child: Container(
               decoration: BoxDecoration(
@@ -41,7 +53,9 @@ class HomeTimerFuncButtonListContainer extends ConsumerWidget {
               child: Padding(
                 padding: const EdgeInsets.all(20),
                 child: SvgPicture.asset(
-                  "assets/image/home_icon/pause_icon.svg",
+                  timerState[timerIndex].timerState == TimerState.started
+                      ? "assets/image/home_icon/pause_icon.svg"
+                      : "assets/image/home_icon/play_filled_icon.svg",
                   width: 40,
                   height: 40,
                   color: MaeumgagymColor.white,
