518f5e053e48bdd4dea58b5a43dc7b72bc2c2a06
ljyo2o9 <yggosxoljy@gmail.com>
2024-02-20 14:07:41 +0900

    ➕ :: [#288] 지금까지 연결했던 dio 없애고 dio_di에서 Dio 받기

--- lib/sign_up/data/data_source/remote/maeumgagym_login_remote_data_source.dart
+++ lib/sign_up/data/data_source/remote/maeumgagym_login_remote_data_source.dart
@@ -1,12 +1,9 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import '../../../domain/model/maeumgagym_login_model.dart';
 
-import 'package:maeum_ga_gym_flutter/core/base_url.dart';
+import 'package:maeum_ga_gym_flutter/core/di/dio_di.dart';
 
 class MaeumgagymLoginRemoteDataSource {
-  Dio dio = Dio(BaseOptions(baseUrl: baseUrl));
-
   Future<MaeumgagymLoginModel> googleLogin(String googleToken) async {
     try {
       return await dio.get('/google/login', queryParameters: {
