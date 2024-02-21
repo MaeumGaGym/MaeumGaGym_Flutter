07225cf953f53fc63640f11807a735ac858514c6
ljyo2o9 <yggosxoljy@gmail.com>
2024-02-21 14:53:40 +0900

    ⚓️ :: [#292] CSRFToken에 TokenSecureStorageDi 연결

--- lib/core/csrf_token/presentation/csrf_token_provider.dart
+++ lib/core/csrf_token/presentation/csrf_token_provider.dart
@@ -3,14 +3,15 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maeum_ga_gym_flutter/core/csrf_token/data/csrf_token_repository_impl.dart';
 import 'package:maeum_ga_gym_flutter/core/csrf_token/domain/csrf_token_model.dart';
 import 'package:maeum_ga_gym_flutter/core/csrf_token/domain/csrf_token_usecase.dart';
+import 'package:maeum_ga_gym_flutter/core/di/token_secure_storage_di.dart';
 
 final csrfTokenController =
-    StateNotifierProvider<CSRFTokenStateNotifier, CSRFTokenState>((ref) {
+StateNotifierProvider<CSRFTokenStateNotifier, CSRFTokenState>((ref) {
   return CSRFTokenStateNotifier();
 });
 
 class CSRFTokenStateNotifier extends StateNotifier<CSRFTokenState> {
-  CSRFTokenStateNotifier() : super(CSRFTokenState(xsrfToken: ''));
+  CSRFTokenStateNotifier() : super(CSRFTokenState(statusCode: 0));
 
   final CSRFTokenUseCase _useCase = CSRFTokenUseCase(CSRFTokenRepositoryImpl());
 
@@ -19,12 +20,13 @@ class CSRFTokenStateNotifier extends StateNotifier<CSRFTokenState> {
 
     debugPrint(xsrfTokenModel.xsrfToken);
 
-    state = CSRFTokenState(xsrfToken: xsrfTokenModel.xsrfToken);
+    await TokenSecureStorageDi.writeXSRFToken(xsrfTokenModel.xsrfToken);
+    state = CSRFTokenState(statusCode: xsrfTokenModel.statusCode);
   }
 }
 
 class CSRFTokenState {
-  String xsrfToken;
+  int statusCode;
 
-  CSRFTokenState({required this.xsrfToken});
+  CSRFTokenState({required this.statusCode});
 }
