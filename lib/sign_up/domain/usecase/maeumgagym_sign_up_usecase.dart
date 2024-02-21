afdc755a1fc369d72bac61205a4d480e487ef1d3
ljyo2o9 <yggosxoljy@gmail.com>
2024-02-21 13:43:55 +0900

    ⚓️ :: [#290] MaeumgagymSignUpUseCase 생성

---
+++ lib/sign_up/domain/usecase/maeumgagym_sign_up_usecase.dart
@@ -0,0 +1,19 @@
+import 'package:maeum_ga_gym_flutter/sign_up/domain/repository/maeumgagym_sign_up_repository.dart';
+
+import '../model/maeumgagym_sign_up_model.dart';
+
+class MaeumgagymSignUpUseCase {
+  final MaeumgagymSignUpRepository _repository;
+
+  MaeumgagymSignUpUseCase(this._repository);
+
+  Future<MaeumgagymSignUpModel> googleSignUp(
+      String googleToken, String xsrfToken, String name) async {
+    return await _repository.googleSignUp(googleToken, xsrfToken, name);
+  }
+
+  Future<MaeumgagymSignUpModel> kakaoSignUp(
+      String googleToken, String xsrfToken, String name) async {
+    return await _repository.kakaoSignUp(googleToken, xsrfToken, name);
+  }
+}
