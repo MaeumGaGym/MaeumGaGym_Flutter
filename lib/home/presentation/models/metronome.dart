42b4ecc1891be619332625e0370b7f4857d2bc6b
유지우 <treeyu12345@naver.com>
2024-02-21 21:13:49 +0900

    🗒️ :: [#275] 메트로놈 모델 추가

---
+++ lib/home/presentation/models/metronome.dart
@@ -0,0 +1,32 @@
+import 'package:maeum_ga_gym_flutter/sign_up/presentation/widget/on_boarding_data.dart';
+
+class Metronome {
+  final double volume;
+  final int beat;
+  final int bpm;
+  final bool onVibration;
+  final bool onPlay;
+
+  Metronome(
+      {required this.volume,
+      required this.beat,
+      required this.bpm,
+      required this.onVibration,
+      required this.onPlay});
+
+  Metronome copyWith({
+    double? volume,
+    int? beat,
+    int? bpm,
+    bool? onVibration,
+    bool? onPlay,
+  }) {
+    return Metronome(
+      volume: volume ?? this.volume,
+      beat: beat ?? this.beat,
+      bpm: bpm ?? this.bpm,
+      onPlay: onPlay ?? this.onPlay,
+      onVibration: onVibration ?? this.onVibration,
+    );
+  }
+}
