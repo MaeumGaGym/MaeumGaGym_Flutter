a11bed429b0455a840c607ec0f4f3c8b13ff1fc5
유지우 <treeyu12345@naver.com>
2024-02-26 23:02:02 +0900

    💉 :: [#297] Metronome Model 값 추가 + 타입 변경

--- lib/home/presentation/models/metronome.dart
+++ lib/home/presentation/models/metronome.dart
@@ -1,30 +1,39 @@
 class Metronome {
-  final double volume;
-  final int beat;
+  final int volume;
+  final int initialBeat;
+  final int currentBeat;
   final int bpm;
   final bool onVibration;
   final bool onPlay;
+  final bool onVolume;
 
-  Metronome(
-      {required this.volume,
-      required this.beat,
-      required this.bpm,
-      required this.onVibration,
-      required this.onPlay});
+  Metronome({
+    required this.volume,
+    required this.initialBeat,
+    required this.currentBeat,
+    required this.bpm,
+    required this.onVibration,
+    required this.onPlay,
+    required this.onVolume,
+  });
 
   Metronome copyWith({
-    double? volume,
-    int? beat,
+    int? volume,
+    int? initialBeat,
+    int? currentBeat,
     int? bpm,
     bool? onVibration,
     bool? onPlay,
+    bool? onVolume,
   }) {
     return Metronome(
       volume: volume ?? this.volume,
-      beat: beat ?? this.beat,
+      initialBeat: initialBeat ?? this.initialBeat,
+      currentBeat: currentBeat ?? this.currentBeat,
       bpm: bpm ?? this.bpm,
       onPlay: onPlay ?? this.onPlay,
       onVibration: onVibration ?? this.onVibration,
+      onVolume: onVolume ?? this.onVolume,
     );
   }
 }
