c0d281cefde7e8236a861d8560cdd840d0345ce3
ljyo2o9 <yggosxoljy@gmail.com>
2024-02-29 14:46:10 +0900

    🗒️ :: [#310] TimerView 완성

--- lib/home/presentation/widget/timer/container/home_timer_circular_timer_container.dart
+++ lib/home/presentation/widget/timer/container/home_timer_circular_timer_container.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
+import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
 import 'package:maeum_ga_gym_flutter/home/presentation/providers/timer_state_provider.dart';
 
 class HomeTimerCircularTimerContainer extends ConsumerStatefulWidget {
@@ -17,14 +21,64 @@ class _HomeTimerCircularTimerContainerState
   Widget build(BuildContext context) {
     final timerState = ref.watch(timersProvider);
     final timerIndex = ref.watch(selectedTimerProvider);
-    return SizedBox(
-      height: MediaQuery.of(context).size.width - 80,
-      width: MediaQuery.of(context).size.width - 80,
-      child: CircularProgressIndicator(
-        color: Colors.red,
-        value: timerState[timerIndex].currentTime.inMicroseconds /
-            timerState[timerIndex].initialTime.inMicroseconds,
-      ),
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.width - 80,
+          width: MediaQuery.of(context).size.width - 80,
+          child: CircularProgressIndicator(
+            color: MaeumgagymColor.blue400,
+            value: timerState[timerIndex].currentTime.inMicroseconds /
+                timerState[timerIndex].initialTime.inMicroseconds,
+            strokeWidth: 6,
+            backgroundColor: MaeumgagymColor.gray100,
+          ),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.width - 249,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              PtdTextWidget.bodyLarge(
+                formatInitialTime(timerState[timerIndex].initialTime),
+                MaeumgagymColor.black,
+              ),
+              SizedBox(
+                height: 64,
+                child: FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: PtdTextWidget.onTimerAndMetronomeTitle(
+                    formatCurrentTime(timerState[timerIndex].currentTime),
+                    MaeumgagymColor.black,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SvgPicture.asset(
+                      'assets/image/home_icon/bell_icon.svg',
+                      color: MaeumgagymColor.gray400,
+                      width: 20,
+                      height: 20,
+                    ),
+                    const SizedBox(width: 4),
+                    PtdTextWidget.bodyMedium(
+                      formatFinishTime(timerState[timerIndex].currentTime),
+                      MaeumgagymColor.gray400,
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
