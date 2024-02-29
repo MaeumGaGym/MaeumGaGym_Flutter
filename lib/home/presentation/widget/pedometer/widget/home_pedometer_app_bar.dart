72236d71af9b9cf6570ecc82121ee1dddaded5d0
유지우 <treeyu12345@naver.com>
2024-02-29 19:11:09 +0900

    🗒️ :: [#303] HomePedometerAppBar 추가

---
+++ lib/home/presentation/widget/pedometer/widget/home_pedometer_app_bar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
+import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
+
+class HomePedometerAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
+  const HomePedometerAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: PreferredSize(
+        preferredSize: preferredSize,
+        child: AppBar(
+          automaticallyImplyLeading: false,
+          scrolledUnderElevation: 0,
+          centerTitle: true,
+          backgroundColor: MaeumgagymColor.white,
+          leading: Center(
+            child: GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: SvgPicture.asset(
+                "assets/image/core_icon/left_arrow_icon.svg",
+                width: 32,
+                height: 32,
+                color: MaeumgagymColor.black,
+              ),
+            ),
+          ),
+          title: PtdTextWidget.labelLarge(
+            "만보기",
+            MaeumgagymColor.black,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(48);
+}
