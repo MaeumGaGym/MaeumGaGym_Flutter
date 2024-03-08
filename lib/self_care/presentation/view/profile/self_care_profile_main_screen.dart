9edefab3c973abb27cc048b4f69d9e718cc0608e
유지우 <treeyu12345@naver.com>
2024-03-08 16:46:31 +0900

    🍗 :: [#344] SelfCareProfileMainScreen 리팩토링

--- lib/self_care/presentation/view/profile/self_care_profile_main_screen.dart
+++ lib/self_care/presentation/view/profile/self_care_profile_main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
+import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/profile/container/self_care_profile_main_info_widget_container.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/profile/container/self_care_profile_main_setting_container.dart';
 
@@ -17,22 +18,32 @@ class SelfCareProfileMainScreen extends StatelessWidget {
       appBar: const SelfCareDefaultAppBar(
         iconPath: "assets/image/core_icon/left_arrow_icon.svg",
       ),
-      body: const SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SelfCareDefaultTitleContainer(
-                  title: "내 프로필",
-                  subTitle: "내 프로필을 관리해보세요.",
-                ),
-                SizedBox(height: 32),
-                SelfCareProfileMainInfoWidgetContainer(),
-                SizedBox(height: 32),
-                SelfCareProfileMainSettingContainer(),
-              ],
+      body: SafeArea(
+        child: Center(
+          child: FittedBox(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 120,
+                    height: 120,
+                    decoration: BoxDecoration(
+                        color: MaeumgagymColor.gray100, shape: BoxShape.circle),
+                  ),
+                  const SizedBox(height: 16),
+                  PtdTextWidget.titleMedium(
+                    "조현석",
+                    MaeumgagymColor.black,
+                  ),
+                  const SizedBox(height: 32),
+                  const SelfCareProfileMainInfoWidgetContainer(),
+                  const SizedBox(height: 32),
+                  const SelfCareProfileMainSettingContainer(),
+                  const SizedBox(height: 31),
+                ],
+              ),
             ),
           ),
         ),
