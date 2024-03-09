e2962d380d3fa7947a796e8d092c733179d2ef9c
ljyo2o9 <yggosxoljy@gmail.com>
2024-03-09 12:37:34 +0900

    🏃🏿‍♂️ :: [#347] tab_body_part_screen 수정
    import문 수정

--- lib/core/component/pose/widget/tab_body/tab_body_part_screen.dart
+++ lib/core/component/pose/widget/tab_body/tab_body_part_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// Provider
-import '../../provider/pose_part_selector_controller.dart';
+import '../../provider/pose_part_selector_provider.dart';
 
 /// Widget
 import 'package:maeum_ga_gym_flutter/core/component/pose/widget/tab_body/part/pose_part_selector.dart';
