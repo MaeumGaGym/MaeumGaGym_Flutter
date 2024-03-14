a3d15e09928586b4e11170d28a94f2571e59e5d3
ljyo2o9 <yggosxoljy@gmail.com>
2024-03-14 15:06:14 +0900

    🚫 :: [#370] MaeumgagymSignUpModel xsrfToken 삭제
    사용하지 않는 xsrfToken을 마음가짐 signUp model에서 삭제하였습니다

--- lib/sign_up/domain/model/maeumgagym_sign_up_model.dart
+++ lib/sign_up/domain/model/maeumgagym_sign_up_model.dart
@@ -1,17 +1,9 @@
-import 'package:dio/dio.dart';
-
 class MaeumgagymSignUpModel {
-  final String xsrfToken;
   final int statusCode;
 
-  MaeumgagymSignUpModel({required this.xsrfToken, required this.statusCode});
+  MaeumgagymSignUpModel({required this.statusCode});
 
-  factory MaeumgagymSignUpModel.fromJson(Headers json, int statusCode) {
-    String jsonXSRFToken = json.map['set-cookie']![0];
-    return MaeumgagymSignUpModel(
-      xsrfToken: jsonXSRFToken.substring(
-          jsonXSRFToken.indexOf('=') + 1, jsonXSRFToken.indexOf(';')),
-      statusCode: statusCode,
-    );
+  factory MaeumgagymSignUpModel.fromJson(int statusCode) {
+    return MaeumgagymSignUpModel(statusCode: statusCode);
   }
 }
