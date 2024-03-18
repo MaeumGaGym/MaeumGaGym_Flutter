b630a8e1c92bd3cd32b8fe403c1f5991a814d098
유지우 <treeyu12345@naver.com>
2024-03-18 21:42:09 +0900

    FEAT :: [#376] 마음가짐 TextStyle 추가

---
+++ lib/core/component/maeungagym_text_style.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
+
+class MaeumGaGymTextStyle {
+  static TextStyle bodyMedium(Color color) {
+    return TextStyle(
+      fontFamily: pretendard,
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      color: color,
+      overflow: TextOverflow.visible,
+      decorationThickness: 0,
+    );
+  }
+
+  static TextStyle bodyLarge(Color color) {
+    return TextStyle(
+      fontFamily: pretendard,
+      fontSize: 20,
+      fontWeight: FontWeight.w400,
+      color: color,
+      overflow: TextOverflow.visible,
+      decorationThickness: 0,
+    );
+  }
+}
