7e6187555adb952b421dbd76535baee2b0d00cf5
ljyo2o9 <yggosxoljy@gmail.com>
2024-03-18 19:47:07 +0900

    ENCHANT :: [#385] maeumgagym_re_issue_remote_data_source 수정
    바뀐 MaeumgagymReIssueModel에 맞춰 수정

--- lib/sign_up/data/data_source/remote/maeumgagym_re_issue_remote_data_source.dart
+++ lib/sign_up/data/data_source/remote/maeumgagym_re_issue_remote_data_source.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maeum_ga_gym_flutter/core/di/dio_di.dart';
 
 import '../../../domain/model/maeumgagym_re_issue_model.dart';
@@ -9,23 +10,17 @@ class MaeumgagymReIssueRemoteDataSource {
 
     try {
       return await dio.get('/auth/re-issue', data: data).then((response) {
-        debugPrint('/auth/re-issue success!');
-        debugPrint(response.statusCode.toString());
         return MaeumgagymReIssueModel.fromJson(
           response.headers,
-          response.statusCode ?? 401,
+          response.statusCode ?? 0,
         );
       });
     } catch (err) {
-      if (err.toString().contains('401')) {
-        return MaeumgagymReIssueModel(
-          statusCode: 401,
-          accessToken: null,
-          refreshToken: null,
-        );
-      } else {
-        throw Exception(err);
-      }
+      return MaeumgagymReIssueModel(
+        stateus: AsyncError(err, StackTrace.empty),
+        accessToken: null,
+        refreshToken: null,
+      );
     }
   }
 }
