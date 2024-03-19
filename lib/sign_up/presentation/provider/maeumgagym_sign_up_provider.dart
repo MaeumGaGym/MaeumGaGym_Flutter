c12d366626dc563a8c2fce74f3626aa4b3fa5354
ljyo2o9 <yggosxoljy@gmail.com>
2024-03-19 17:04:20 +0900

    FEAT :: [#392] maeumgagym_sign_up AsyncValue<int> 적용
    서버 통신 상태를 확인하기 위해 MaeumgagymSignUpModel에서 AsyncValue<int> 로 수정

--- lib/sign_up/presentation/provider/maeumgagym_sign_up_provider.dart
+++ lib/sign_up/presentation/provider/maeumgagym_sign_up_provider.dart
@@ -1,42 +1,31 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maeum_ga_gym_flutter/sign_up/data/repositoryImpl/maeumgagym_sign_up_repository_impl.dart';
-import 'package:maeum_ga_gym_flutter/sign_up/domain/model/maeumgagym_sign_up_model.dart';
 import 'package:maeum_ga_gym_flutter/sign_up/domain/usecase/maeumgagym_sign_up_usecase.dart';
 
 final maeumgagymSignUpController =
-    StateNotifierProvider<MaeumgagymSignUpStateNotifier, MaeumgagymSignUpState>(
+    StateNotifierProvider<MaeumgagymSignUpStateNotifier, AsyncValue<int?>>(
         (ref) {
   return MaeumgagymSignUpStateNotifier();
 });
 
-class MaeumgagymSignUpStateNotifier
-    extends StateNotifier<MaeumgagymSignUpState> {
-  MaeumgagymSignUpStateNotifier() : super(MaeumgagymSignUpState(statusCode: 0));
+class MaeumgagymSignUpStateNotifier extends StateNotifier<AsyncValue<int?>> {
+  MaeumgagymSignUpStateNotifier() : super(const AsyncData(500));
 
   final MaeumgagymSignUpUseCase _useCase =
       MaeumgagymSignUpUseCase(MaeumgagymSignUpRepositoryImpl());
 
   Future<void> googleSignUp(String googleToken, String name) async {
-    MaeumgagymSignUpModel signUpXsrfToken =
+    state = const AsyncLoading();
+    AsyncValue<int?> signUpState =
         await _useCase.googleSignUp(googleToken, name);
 
-    state = MaeumgagymSignUpState(
-      statusCode: signUpXsrfToken.statusCode,
-    );
+    state = signUpState;
   }
 
   Future<void> kakaoSignUp(String googleToken, String name) async {
-    MaeumgagymSignUpModel signUpXsrfToken =
+    AsyncValue<int?> signUpState =
         await _useCase.kakaoSignUp(googleToken, name);
 
-    state = MaeumgagymSignUpState(
-      statusCode: signUpXsrfToken.statusCode,
-    );
+    state = signUpState;
   }
-}
-
-class MaeumgagymSignUpState {
-  final int statusCode;
-
-  MaeumgagymSignUpState({required this.statusCode});
 }
