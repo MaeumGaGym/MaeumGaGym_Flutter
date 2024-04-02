b7ce515bb7fcfbde59343adc08b509d9fdeae078
유지우 <treeyu12345@naver.com>
2024-04-02 17:06:53 +0900

    FEAT :: [#402] RoutineRepositoryImpl 구현

---
+++ lib/self_care/data/repositoryImpl/routine_repository_impl.dart
@@ -0,0 +1,91 @@
+import 'package:maeum_ga_gym_flutter/self_care/data/date_source/remote/routine_remote_data_source.dart';
+import 'package:maeum_ga_gym_flutter/self_care/domain/model/exercise_info_model.dart';
+import 'package:maeum_ga_gym_flutter/self_care/domain/model/routine_history_model.dart';
+import 'package:maeum_ga_gym_flutter/self_care/domain/model/routine_response_model.dart';
+import 'package:maeum_ga_gym_flutter/self_care/domain/repository/routine_repository.dart';
+import 'package:riverpod/src/common.dart';
+
+class RoutineRepositoryImpl implements RoutineRepository {
+  final RoutineRemoteDataSource _remoteDataSource;
+
+  RoutineRepositoryImpl(this._remoteDataSource);
+
+  @override
+  Future<RoutineResponseModel> getTodayRoutine({
+    required String accessToken,
+  }) async {
+    return await _remoteDataSource.getTodayRoutine(
+      accessToken: accessToken,
+    );
+  }
+
+  @override
+  Future<AsyncValue<int?>> completeTodayRoutines({
+    required String accessToken,
+  }) async {
+    return await _remoteDataSource.completeTodayRoutines(
+      accessToken: accessToken,
+    );
+  }
+
+  @override
+  Future<AsyncValue<int?>> createRoutine({
+    required String accessToken,
+    required String routineName,
+    required bool isArchived,
+    required bool isShared,
+    required List<ExerciseInfoModel> exerciseInfoModelList,
+    List<String>? dayOfWeeks,
+  }) async {
+    return await _remoteDataSource.createRoutine(
+        accessToken: accessToken,
+        routineName: routineName,
+        isArchived: isArchived,
+        isShared: isShared,
+        exerciseInfoModelList: exerciseInfoModelList,
+        dayOfWeeks: dayOfWeeks);
+  }
+
+  @override
+  Future<AsyncValue<int?>> deleteRoutine({
+    required String accessToken,
+    required String routineId,
+  }) async {
+    return await _remoteDataSource.deleteRoutine(
+      accessToken: accessToken,
+      routineId: routineId,
+    );
+  }
+
+  @override
+  Future<AsyncValue<int?>> editRoutine({
+    required String accessToken,
+    required String routineName,
+    required bool isArchived,
+    required bool isShared,
+    List<ExerciseInfoModel>? exerciseInfoModelList,
+    required int routineId,
+    List<String>? dayOfWeeks,
+  }) async {
+    return await _remoteDataSource.editRoutine(
+      accessToken: accessToken,
+      routineName: routineName,
+      isArchived: isArchived,
+      isShared: isShared,
+      exerciseInfoModelList: exerciseInfoModelList,
+      routineId: routineId,
+      dayOfWeeks: dayOfWeeks,
+    );
+  }
+
+  @override
+  Future<RoutineHistoryModel> getRoutineHistory({
+    required String accessToken,
+    required String date,
+  }) async {
+    return await _remoteDataSource.getRoutineHistory(
+      accessToken: accessToken,
+      date: date,
+    );
+  }
+}
