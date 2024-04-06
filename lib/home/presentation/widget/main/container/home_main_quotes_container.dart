de3e4aabfa46936962f08463f2859aac4e610be7
ljyo2o9 <yggosxoljy@gmail.com>
2024-04-06 21:41:35 +0900

    DOC :: home_main_quotes_container 생성

---
+++ lib/home/presentation/widget/main/container/home_main_quotes_container.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../../config/maeumgagym_color.dart';
+import '../../../../../core/component/text/pretendard/ptd_text_widget.dart';
+import '../../../providers/home_quotes_provider.dart';
+
+class HomeMainQuotesContainer extends ConsumerWidget {
+  const HomeMainQuotesContainer({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Builder(
+      builder: (context) => ref.watch(homeQuotesController).statusCode.when(
+        data: (data) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 12),
+                child: PtdTextWidget.titleMedium(
+                  ref.watch(homeQuotesController).message.toString(),
+                  MaeumgagymColor.blue500,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Padding(
+                padding: const EdgeInsets.only(left: 12),
+                child: PtdTextWidget.labelMedium(
+                  ref.watch(homeQuotesController).author.toString(),
+                  MaeumgagymColor.gray500,
+                ),
+              ),
+            ],
+          );
+        },
+        error: (err, _) {
+          return const SizedBox();
+        },
+        loading: () {
+          return const SizedBox();
+        },
+      ),
+    );
+  }
+}
