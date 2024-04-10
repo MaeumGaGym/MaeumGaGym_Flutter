49e96c6f902571faff715d74f7f969a509fc1dec
ljyo2o9 <yggosxoljy@gmail.com>
2024-04-10 14:30:29 +0900

    TRUCK :: 파일 위치 변경

--- lib/core/re_issue/domain/maeumgagym_re_issue_repository.dart
+++ lib/core/re_issue/domain/maeumgagym_re_issue_repository.dart
@@ -1,4 +1,4 @@
-import '../model/maeumgagym_re_issue_model.dart';
+import 'maeumgagym_re_issue_model.dart';
 
 abstract class MaeumgagymReIssueRepository {
   Future<MaeumgagymReIssueModel> getReIssue(String refreshToken);
