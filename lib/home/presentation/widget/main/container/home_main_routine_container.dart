0f98e44f219980127b3baf20bb78402c9ad9ff0e
ljyo2o9 <yggosxoljy@gmail.com>
2024-04-12 08:44:40 +0900

    DOC :: 오늘의 루틴이 없을때 생기는 뷰 추가

--- lib/home/presentation/widget/main/container/home_main_routine_container.dart
+++ lib/home/presentation/widget/main/container/home_main_routine_container.dart
@@ -12,116 +12,118 @@ class HomeMainRoutineContainer extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Builder(
-      builder: (context) =>
-          ref.watch(homeTodayRoutineController).statusCode.when(
-        data: (data) {
-          return Container(
-            decoration: BoxDecoration(
-              color: MaeumgagymColor.white,
-              borderRadius: BorderRadius.circular(16),
+    return Container(
+      decoration: BoxDecoration(
+        color: MaeumgagymColor.white,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 24),
+              child: HomeMainContainerTitle(title: "오늘의 루틴"),
             ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 24),
-                    child: HomeMainContainerTitle(title: "오늘의 루틴"),
-                  ),
-                  ListView.builder(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: ref
-                        .watch(homeTodayRoutineController)
-                        .exerciseInfoResponseList!
-                        .length,
-                    itemBuilder: (context, index) {
-                      // String key = routineData.keys.elementAt(index);
-                      final poseData = ref
-                          .read(homeTodayRoutineController)
-                          .exerciseInfoResponseList![index];
-                      return Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 15, vertical: 8),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            ref.watch(homeTodayRoutineController).statusCode.when(data: (data) {
+              return ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: ref
+                    .watch(homeTodayRoutineController)
+                    .exerciseInfoResponseList!
+                    .length,
+                itemBuilder: (context, index) {
+                  // String key = routineData.keys.elementAt(index);
+                  final poseData = ref
+                      .read(homeTodayRoutineController)
+                      .exerciseInfoResponseList![index];
+                  return Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 15, vertical: 8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
                               children: [
-                                Row(
+                                Container(
+                                  height: 48,
+                                  width: 48,
+                                  decoration: BoxDecoration(
+                                    color: MaeumgagymColor.gray100,
+                                    shape: BoxShape.circle,
+                                  ),
+                                  child: ImageWidget(
+                                    image: poseData.pose.thumbnail!,
+                                    imageType: ImageType.pngNetwork,
+                                  ),
+                                ),
+                                const SizedBox(width: 10),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    Container(
-                                      height: 48,
-                                      width: 48,
-                                      decoration: BoxDecoration(
-                                        color: MaeumgagymColor.gray100,
-                                        shape: BoxShape.circle,
-                                      ),
-                                      child: ImageWidget(
-                                        image: poseData.pose.thumbnail!,
-                                        imageType: ImageType.pngNetwork,
-                                      ),
+                                    PtdTextWidget.titleSmall(
+                                      poseData.pose.name!,
+                                      MaeumgagymColor.black,
                                     ),
-                                    const SizedBox(width: 10),
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        PtdTextWidget.titleSmall(
-                                          poseData.pose.name!,
-                                          MaeumgagymColor.black,
-                                        ),
-                                        const SizedBox(height: 2),
-                                        PtdTextWidget.bodySmall(
-                                          "${poseData.repetitions}개 | ${poseData.sets}세트",
-                                          MaeumgagymColor.gray400,
-                                        ),
-                                      ],
+                                    const SizedBox(height: 2),
+                                    PtdTextWidget.bodySmall(
+                                      "${poseData.repetitions}개 | ${poseData.sets}세트",
+                                      MaeumgagymColor.gray400,
                                     ),
                                   ],
                                 ),
-                                GestureDetector(
-                                  child: SvgPicture.asset(
-                                    'assets/image/core_icon/right_arrow_icon.svg',
-                                    width: 24,
-                                    height: 24,
-                                  ),
-                                ),
                               ],
                             ),
-                          ),
-                          // SizedBox(height: index == .length - 1 ? 0 : 8),
-                          SizedBox(
-                              height: index ==
-                                      ref
-                                              .read(homeTodayRoutineController)
-                                              .exerciseInfoResponseList!
-                                              .length -
-                                          1
-                                  ? 0
-                                  : 8),
-                        ],
-                      );
-                    },
+                            GestureDetector(
+                              child: SvgPicture.asset(
+                                'assets/image/core_icon/right_arrow_icon.svg',
+                                width: 24,
+                                height: 24,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      // SizedBox(height: index == .length - 1 ? 0 : 8),
+                      SizedBox(
+                          height: index ==
+                                  ref
+                                          .read(homeTodayRoutineController)
+                                          .exerciseInfoResponseList!
+                                          .length -
+                                      1
+                              ? 0
+                              : 8),
+                    ],
+                  );
+                },
+              );
+            }, error: (err, _) {
+              if (err.toString().contains("type 'Null' is not")) {
+                return Padding(
+                  padding: const EdgeInsets.only(top: 24),
+                  child: PtdTextWidget.bodyLarge(
+                    '오늘의 루틴이 없어요',
+                    MaeumgagymColor.gray400,
                   ),
-                  const SizedBox(height: 16),
-                ],
-              ),
-            ),
-          );
-        },
-        error: (err, _) {
-          return Text(err.toString());
-        },
-        loading: () {
-          return Center(
-            child: CircularProgressIndicator(
-              color: MaeumgagymColor.blue500,
-            ),
-          );
-        },
+                );
+              } else {
+                return Text(err.toString());
+              }
+            }, loading: () {
+              return Center(
+                child: CircularProgressIndicator(
+                  color: MaeumgagymColor.blue500,
+                ),
+              );
+            }),
+            const SizedBox(height: 16),
+          ],
+        ),
       ),
     );
   }
