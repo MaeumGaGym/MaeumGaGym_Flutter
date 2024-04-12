c6c46da1eef1c198e6cca9a1353d10e82cda69c1
ljyo2o9 <yggosxoljy@gmail.com>
2024-04-12 18:41:36 +0900

    FEAT :: PoseAllRepositoryImpl 생성

---
+++ lib/core/component/pose/data/repositoryImpl/pose_all_repository_impl.dart
@@ -0,0 +1,13 @@
+import 'package:maeum_ga_gym_flutter/core/component/pose/data/data_source/remote/pose_all_remote_data_source.dart';
+import 'package:maeum_ga_gym_flutter/core/component/pose/domain/model/pose_data_model.dart';
+import 'package:maeum_ga_gym_flutter/core/component/pose/domain/repository/pose_all_repository.dart';
+
+class PoseAllRepositoryImpl implements PoseAllRepository {
+  final PoseAllRemoteDataSource _dataSource = PoseAllRemoteDataSource();
+
+  @override
+  Future<List<PoseDataModel>> getPoseDataList(
+      {required String lastUpdated}) async {
+    return await _dataSource.getPoseDataList(lastUpdated: lastUpdated);
+  }
+}
