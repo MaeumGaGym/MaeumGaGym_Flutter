0620ecc31296fb6f1d91022a66d98a6b7ea3fb82
ljyo2o9 <yggosxoljy@gmail.com>
2024-04-12 17:14:14 +0900

    BURN :: API가 태그 검색 -> 전체 검색으로 바뀌었기 때문에 원래 있던 Pose_tag~삭제

--- lib/core/component/pose/domain/repository/pose_tag_list_repository.dart
+++
@@ -1,5 +0,0 @@
-import 'package:maeum_ga_gym_flutter/core/component/pose/domain/model/pose_tag_list_model.dart';
-
-abstract class PoseTagListRepository {
-  Future<PoseTagListModel> getTagList({required String tag});
-}
