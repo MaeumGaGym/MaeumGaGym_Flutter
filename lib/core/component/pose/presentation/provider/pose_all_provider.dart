cf87bcf09cc4a8fddb40b464e3414b888942982c
ljyo2o9 <yggosxoljy@gmail.com>
2024-04-12 18:42:11 +0900

    FEAT :: poseAllController 생성

---
+++ lib/core/component/pose/presentation/provider/pose_all_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maeum_ga_gym_flutter/core/component/pose/data/repositoryImpl/pose_all_repository_impl.dart';
+import 'package:maeum_ga_gym_flutter/core/component/pose/domain/model/pose_data_model.dart';
+import 'package:maeum_ga_gym_flutter/core/component/pose/domain/usecase/pose_all_usecase.dart';
+
+final poseAllController =
+    StateNotifierProvider<PoseTagListStateNotifier, List<PoseDataModel>>((ref) {
+  return PoseTagListStateNotifier();
+});
+
+class PoseTagListStateNotifier extends StateNotifier<List<PoseDataModel>> {
+  PoseTagListStateNotifier() : super([]);
+
+  final PoseAllUseCase _useCase = PoseAllUseCase(
+    repository: PoseAllRepositoryImpl(),
+  );
+
+  Future<void> getPoseDataList({required String lastUpdated}) async {
+    state = await _useCase.getPoseDataList(lastUpdated: lastUpdated);
+  }
+}
