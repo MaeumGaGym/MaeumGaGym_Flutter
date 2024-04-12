9b091022a148b218d2f33f4a6e503a7bd638f479
ljyo2o9 <yggosxoljy@gmail.com>
2024-04-12 19:38:37 +0900

    FEAT :: poseSearchScreen에서 이제 이름 뿐 아니라 simplePart로도 검색이 가능함

--- lib/pose/presentation/widget/search/pose_search_body.dart
+++ lib/pose/presentation/widget/search/pose_search_body.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maeum_ga_gym_flutter/core/component/image_widget.dart';
+import 'package:maeum_ga_gym_flutter/core/component/pose/presentation/provider/pose_all_provider.dart';
 import 'package:maeum_ga_gym_flutter/pose/presentation/provider/pose_search_provider.dart';
 
 /// Core
@@ -36,27 +37,26 @@ class PoseSearchBody extends ConsumerWidget {
             child: ListView.builder(
               itemCount: data.length,
               itemBuilder: (BuildContext context, int index) {
+                final poseAll = ref.read(poseAllController);
+
+                final bool searchCondition =
+                    !(([poseAll[index].name!] + poseAll[index].simplePart)
+                        .toString()
+                        .contains(poseSearchState));
+
                 /// 만약 textField에 값에 이름이 들어가지 않는다면 SizedBox.shrink
-                if (poseSearchState.isNotEmpty &&
-                    !(data[index]['simpleName']!.contains(poseSearchState))) {
+                if (poseSearchState.isNotEmpty && searchCondition) {
                   return const SizedBox.shrink();
                 } else {
                   return GestureDetector(
-                    // onTap: () => Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(
-                    //     builder: (context) => PoseDetailScreen(
-                    //       poseImages: data[index]['poseImages'],
-                    //       simpleName: data[index]['simpleName'],
-                    //       exactName: data[index]['exactName'],
-                    //       simplePart: data[index]['simplePart'],
-                    //       exactPart: data[index]['exactPart'],
-                    //       exerciseWay: data[index]['exerciseWay'],
-                    //       caution: data[index]['caution'],
-                    //       breatheWay: data[index]['breatheWay'],
-                    //     ),
-                    //   ),
-                    // ),
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PoseDetailScreen(
+                          id: poseAll[index].id!,
+                        ),
+                      ),
+                    ),
                     child: Container(
                       width: MediaQuery.of(context).size.width - 40,
                       color: Colors.white,
@@ -68,8 +68,8 @@ class PoseSearchBody extends ConsumerWidget {
                         children: [
                           /// Pose Image
                           ImageWidget(
-                            image: data[index]['poseImages'][0],
-                            imageType: ImageType.png,
+                            image: poseAll[index].thumbnail!,
+                            imageType: ImageType.pngNetwork,
                             backgroundColor: MaeumgagymColor.gray50,
                             radiusCircular: 8,
                             width: 64,
@@ -89,13 +89,22 @@ class PoseSearchBody extends ConsumerWidget {
                                 children: [
                                   /// SimpleName
                                   PtdTextWidget.bodyMedium(
-                                    data[index]['simpleName'],
+                                    poseAll[index].name!,
                                     MaeumgagymColor.black,
                                   ),
 
                                   /// ExactPart
                                   PtdTextWidget.bodyMedium(
-                                    data[index]['exactPart'],
+                                    poseAll[index]
+                                        .simplePart
+                                        .toString()
+                                        .substring(
+                                            1,
+                                            poseAll[index]
+                                                    .simplePart
+                                                    .toString()
+                                                    .length -
+                                                1),
                                     MaeumgagymColor.gray400,
                                   )
                                 ],
