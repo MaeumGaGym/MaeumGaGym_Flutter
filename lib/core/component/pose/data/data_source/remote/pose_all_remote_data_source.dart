f5090eaabea240f9dbd1d6588e613079791d841a
ljyo2o9 <yggosxoljy@gmail.com>
2024-04-14 23:14:32 +0900

    FEAT :: PoseAllRemoteDataSource에 lastUpdated추가

--- lib/core/component/pose/data/data_source/remote/pose_all_remote_data_source.dart
+++ lib/core/component/pose/data/data_source/remote/pose_all_remote_data_source.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:maeum_ga_gym_flutter/core/component/pose/data/data_source/local/maeumgagym_pose_data_version_local_data_source.dart';
 import 'package:maeum_ga_gym_flutter/core/component/pose/data/data_source/local/pose_all_local_data_source.dart';
 import 'package:maeum_ga_gym_flutter/core/component/pose/domain/model/pose_data_model.dart';
 import 'package:maeum_ga_gym_flutter/core/di/dio_di.dart';
@@ -10,20 +11,23 @@ import '../../../domain/model/pose_data_list_model.dart';
 class PoseAllRemoteDataSource {
   final PoseAllLocalDataSource _poseAllLocalDataSource =
       PoseAllLocalDataSource();
+  final MaeumgagymPoseDataVersionLocalDataSource
+      _poseDataVersionLocalDataSource =
+      MaeumgagymPoseDataVersionLocalDataSource();
 
-  Future<List<PoseDataModel>> getPoseDataList(
-      {required String lastUpdated}) async {
+  Future<List<PoseDataModel>> getPoseDataList() async {
     final accessToken = await TokenSecureStorageDi.readLoginAccessToken();
     Map<String, dynamic> headers = {
       'Content-Type': 'application/json',
       'Authorization': accessToken,
     };
 
-    Map<String, String> data = {
-      "last_updated": lastUpdated,
-    };
+    final String? lastUpdated =
+        await _poseDataVersionLocalDataSource.getPoseDataVersion();
 
-    debugPrint(lastUpdated);
+    Map<String, String> data = {
+      "last_updated": lastUpdated ?? "2022-12-15T09:00",
+    };
 
     try {
       final response = await dio.get(
@@ -32,10 +36,14 @@ class PoseAllRemoteDataSource {
         options: Options(headers: headers),
       );
 
+      debugPrint("poseAll -- ${response.statusCode}");
+
       if (response.statusCode! == 200) {
         await _poseAllLocalDataSource.setPoseDataList(
           PoseDataListModel.fromJson(response.data),
         );
+
+        await _poseDataVersionLocalDataSource.setPoseDataVersion();
 
         return await _poseAllLocalDataSource.getPoseDataList();
       } else {
