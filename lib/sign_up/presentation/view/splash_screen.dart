d350f2e9ca1760f82df72067975196995bfb93c9
ljyo2o9 <yggosxoljy@gmail.com>
2024-04-14 14:50:34 +0900

    FEAT :: API 연동

--- lib/sign_up/presentation/view/splash_screen.dart
+++ lib/sign_up/presentation/view/splash_screen.dart
@@ -5,6 +5,7 @@ import 'package:go_router/go_router.dart';
 import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
 import 'package:maeum_ga_gym_flutter/core/component/image_widget.dart';
 import 'package:maeum_ga_gym_flutter/core/component/pose/presentation/provider/pose_all_provider.dart';
+import 'package:maeum_ga_gym_flutter/core/component/pose/presentation/provider/pose_recommend_provider.dart';
 
 import '../../../core/di/login_option_di.dart';
 import '../../../core/di/token_secure_storage_di.dart';
@@ -52,6 +53,8 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
             await ref
                 .read(poseAllController.notifier)
                 .getPoseDataList(lastUpdated: "2022-12-15T09:00");
+
+            await ref.read(poseRecommendController.notifier).getRecommendPose();
 
             context.go('/home');
           },
