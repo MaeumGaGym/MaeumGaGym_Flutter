4d846a2579944e38196b8bc98df0761c6da8bc57
ljyo2o9 <yggosxoljy@gmail.com>
2024-04-18 10:26:21 +0900

    FEAT :: 오늘의 루틴이 비어있다면 TodayRoutineScreen으로 들어가지 못하도록 만듬

--- lib/home/presentation/widget/main/container/home_main_routine_container.dart
+++ lib/home/presentation/widget/main/container/home_main_routine_container.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
 import 'package:maeum_ga_gym_flutter/core/component/image_widget.dart';
 import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
 import 'package:maeum_ga_gym_flutter/home/presentation/providers/home_today_routines_provider.dart';
+import 'package:maeum_ga_gym_flutter/home/presentation/view/home_today_routine_screen.dart';
 import 'package:maeum_ga_gym_flutter/home/presentation/widget/main/widget/home_main_container_title.dart';
+import 'package:maeum_ga_gym_flutter/pose/presentation/view/pose_detail_screen.dart';
 
 class HomeMainRoutineContainer extends ConsumerWidget {
   const HomeMainRoutineContainer({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    bool errState = ref
+        .watch(homeTodayRoutineController)
+        .statusCode
+        .error
+        .toString()
+        .contains("type 'Null' is not");
+
     return Container(
       decoration: BoxDecoration(
         color: MaeumgagymColor.white,
@@ -22,9 +31,22 @@ class HomeMainRoutineContainer extends ConsumerWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 24),
-              child: HomeMainContainerTitle(title: "오늘의 루틴"),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 24),
+              child: GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: () {
+                  if (!errState) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HomeTodayRoutineScreen(),
+                      ),
+                    );
+                  }
+                },
+                child: const HomeMainContainerTitle(title: "오늘의 루틴"),
+              ),
             ),
             ref.watch(homeTodayRoutineController).statusCode.when(data: (data) {
               return ListView.builder(
@@ -39,71 +61,83 @@ class HomeMainRoutineContainer extends ConsumerWidget {
                   final poseData = ref
                       .read(homeTodayRoutineController)
                       .exerciseInfoResponseList![index];
-                  return Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 15, vertical: 8),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Container(
-                                  height: 48,
-                                  width: 48,
-                                  decoration: BoxDecoration(
-                                    color: MaeumgagymColor.gray100,
-                                    shape: BoxShape.circle,
-                                  ),
-                                  child: ImageWidget(
-                                    image: poseData.pose.thumbnail!,
-                                    imageType: ImageType.pngNetwork,
-                                  ),
-                                ),
-                                const SizedBox(width: 10),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    PtdTextWidget.titleSmall(
-                                      poseData.pose.name!,
-                                      MaeumgagymColor.black,
-                                    ),
-                                    const SizedBox(height: 2),
-                                    PtdTextWidget.bodySmall(
-                                      "${poseData.repetitions}개 | ${poseData.sets}세트",
-                                      MaeumgagymColor.gray400,
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                            GestureDetector(
-                              child: SvgPicture.asset(
-                                'assets/image/core_icon/right_arrow_icon.svg',
-                                width: 24,
-                                height: 24,
-                              ),
-                            ),
-                          ],
+                  return GestureDetector(
+                    behavior: HitTestBehavior.opaque,
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PoseDetailScreen(
+                          id: poseData.pose.id!,
                         ),
                       ),
-                      // SizedBox(height: index == .length - 1 ? 0 : 8),
-                      SizedBox(
-                          height: index ==
-                                  ref
-                                          .read(homeTodayRoutineController)
-                                          .exerciseInfoResponseList!
-                                          .length -
-                                      1
-                              ? 0
-                              : 8),
-                    ],
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 15, vertical: 8),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Container(
+                                    height: 48,
+                                    width: 48,
+                                    decoration: BoxDecoration(
+                                      color: MaeumgagymColor.gray100,
+                                      shape: BoxShape.circle,
+                                    ),
+                                    child: ImageWidget(
+                                      image: poseData.pose.thumbnail!,
+                                      imageType: ImageType.pngNetwork,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 10),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      PtdTextWidget.titleSmall(
+                                        poseData.pose.name!,
+                                        MaeumgagymColor.black,
+                                      ),
+                                      const SizedBox(height: 2),
+                                      PtdTextWidget.bodySmall(
+                                        "${poseData.repetitions}개 | ${poseData.sets}세트",
+                                        MaeumgagymColor.gray400,
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              GestureDetector(
+                                child: SvgPicture.asset(
+                                  'assets/image/core_icon/right_arrow_icon.svg',
+                                  width: 24,
+                                  height: 24,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        // SizedBox(height: index == .length - 1 ? 0 : 8),
+                        SizedBox(
+                            height: index ==
+                                    ref
+                                            .read(homeTodayRoutineController)
+                                            .exerciseInfoResponseList!
+                                            .length -
+                                        1
+                                ? 0
+                                : 8),
+                      ],
+                    ),
                   );
                 },
               );
             }, error: (err, _) {
-              if (err.toString().contains("type 'Null' is not")) {
+              if (errState) {
                 return Padding(
                   padding: const EdgeInsets.only(top: 24),
                   child: PtdTextWidget.bodyLarge(
