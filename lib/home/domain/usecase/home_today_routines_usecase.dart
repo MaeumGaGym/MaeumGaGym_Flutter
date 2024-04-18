a48230b2d32075d9b73431de3523f436891f198a
ljyo2o9 <yggosxoljy@gmail.com>
2024-04-18 10:23:03 +0900

    FEAT :: completeTodayRoutines 추가

--- lib/home/domain/usecase/home_today_routines_usecase.dart
+++ lib/home/domain/usecase/home_today_routines_usecase.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maeum_ga_gym_flutter/home/domain/repository/home_today_routines_repository.dart';
 
 import '../model/home_today_routines_model.dart';
@@ -10,5 +11,9 @@ class HomeTodayRoutinesUseCase {
 
   Future<HomeTodayRoutinesModel> getTodayRoutines() async {
     return await _repository.getTodayRoutines();
+  }
+
+  Future<AsyncValue<int>> completeTodayRoutines() async {
+    return await _repository.completeTodayRoutines();
   }
 }
