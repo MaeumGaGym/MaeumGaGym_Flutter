8ab133aebbe71433c2cec79ef3d34e14cbe3ecf1
유지우 <treeyu12345@naver.com>
2024-04-18 16:12:33 +0900

    BURN :: 불필요한 코드 제거

--- lib/self_care/presentation/view/my_routine/self_care_my_routine_main_screen.dart
+++ lib/self_care/presentation/view/my_routine/self_care_my_routine_main_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/provider/my_routine/self_care_my_routine_all_me_routine_provider.dart';
-import 'package:maeum_ga_gym_flutter/self_care/presentation/provider/my_routine/self_care_my_routine_edit_routine_provider.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/view/my_routine/self_care_my_routine_add_screen.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/view/my_routine/self_care_my_routine_detail_screen.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/my_routine/widget/self_care_my_routine_button.dart';
@@ -101,7 +100,6 @@ class _SelfCareMyRoutineMainScreenState
                       }
                     },
                     error: (error, stack) {
-                      print(error.toString());
                       return Text('에러가 발생했습니다: $error');
                     },
                     loading: () {
