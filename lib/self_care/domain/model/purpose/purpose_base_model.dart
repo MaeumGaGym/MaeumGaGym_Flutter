81817cc5016eb10ddc8da97038d60c3810a0d60a
유지우 <treeyu12345@naver.com>
2024-05-07 20:01:23 +0900

    FEAT :: PurposeBaseModel 생성

---
+++ lib/self_care/domain/model/purpose/purpose_base_model.dart
@@ -0,0 +1,24 @@
+class PurposeBaseModel {
+  final String? title;
+  final String? content;
+  final String? startDate;
+  final String? endDate;
+
+  PurposeBaseModel({
+    required this.title,
+    required this.content,
+    required this.startDate,
+    required this.endDate,
+  });
+
+  factory PurposeBaseModel.fromJson(
+    Map<String, dynamic> json
+  ) {
+    return PurposeBaseModel(
+      title: json['title'],
+      content: json['content'],
+      startDate: json['startDate'],
+      endDate: json['endDate'],
+    );
+  }
+}
