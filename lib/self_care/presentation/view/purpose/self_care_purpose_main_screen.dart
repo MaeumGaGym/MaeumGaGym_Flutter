e1d10e5b4dce7288dcc591e94498d887f30e1233
유지우 <treeyu12345@naver.com>
2024-05-14 19:29:31 +0900

    FEAT :: 내 목표 조회 API 적용
    미완

--- lib/self_care/presentation/view/purpose/self_care_purpose_main_screen.dart
+++ lib/self_care/presentation/view/purpose/self_care_purpose_main_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
+import 'package:maeum_ga_gym_flutter/self_care/domain/model/purpose/purpose_base_model.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/provider/purpose/self_care_purpose_all_purposes_provider.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/view/purpose/self_care_purpose_add_screen.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/view/purpose/self_care_purpose_detail_screen.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/my_routine/widget/self_care_my_routine_button.dart';
@@ -7,11 +10,36 @@ import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/purpose/widge
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/self_care_default_app_bar.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/self_care_default_title_container.dart';
 
-class SelfCarePurposeMainScreen extends StatelessWidget {
+class SelfCarePurposeMainScreen extends ConsumerStatefulWidget {
   const SelfCarePurposeMainScreen({super.key});
 
   @override
+  ConsumerState<SelfCarePurposeMainScreen> createState() => _SelfCarePurposeMainScreenState();
+}
+
+class _SelfCarePurposeMainScreenState extends ConsumerState<SelfCarePurposeMainScreen> {
+  late ScrollController paginationController;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      Duration.zero,
+          () => ref
+          .read(selfCarePurposeAllPurposesProvider.notifier)
+          .getAllPurpose(index: 0),
+    );
+    paginationController = ScrollController()..addListener(() {
+      if (paginationController.position.pixels == paginationController.position.maxScrollExtent) {
+        ref.read(selfCarePurposeAllPurposesProvider.notifier).getAllPurpose(index: ref.watch(selfCarePurposeAllPurposesProvider).purposeList.length);
+      }
+    });
+  }
+
+  
+  @override
   Widget build(BuildContext context) {
+    final purposeAllState = ref.watch(selfCarePurposeAllPurposesProvider);
     return Scaffold(
       backgroundColor: MaeumgagymColor.white,
       appBar: const SelfCareDefaultAppBar(
@@ -19,6 +47,7 @@ class SelfCarePurposeMainScreen extends StatelessWidget {
       ),
       body: SafeArea(
         child: SingleChildScrollView(
+          controller: paginationController,
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
             child: Column(
@@ -32,7 +61,7 @@ class SelfCarePurposeMainScreen extends StatelessWidget {
                 ListView.builder(
                   shrinkWrap: true,
                   physics: const NeverScrollableScrollPhysics(),
-                  itemCount: 30, // 임의로 넣은 아이템 갯수
+                  itemCount: purposeAllState.purposeList.length, // 임의로 넣은 아이템 갯수
                   itemBuilder: (context, index) {
                     return Column(
                       children: [
@@ -43,9 +72,9 @@ class SelfCarePurposeMainScreen extends StatelessWidget {
                                   const SelfCarePurposeDetailScreen(),
                             ),
                           ),
-                          child: const SelfCarePurposeItemWidget(
-                            title: "디자인 완성하기",
-                            subTitle: "12월 26일 ~ 12월 27일",
+                          child: SelfCarePurposeItemWidget(
+                            title: purposeAllState.purposeList[index].title.toString(),
+                            subTitle: purposeAllState.purposeList[index].startDate.toString(),
                           ),
                         ),
                         SizedBox(height: index == 30 - 1 ? 0 : 12),
