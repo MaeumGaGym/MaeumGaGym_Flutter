21193ecaef1834eb8b8fa46ec750167d0624e337
유지우 <treeyu12345@naver.com>
2024-05-30 12:24:23 +0900

    ENCHANT :: id 추가

--- lib/self_care/domain/model/purpose/purpose_response_model.dart
+++ lib/self_care/domain/model/purpose/purpose_response_model.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class PurposeResponseModel {
   final AsyncValue<int> statusCode;
+  final int? id;
   final String? title;
   final String? content;
   final String? startDate;
@@ -9,6 +10,7 @@ class PurposeResponseModel {
 
   PurposeResponseModel({
     required this.statusCode,
+    required this.id,
     required this.title,
     required this.content,
     required this.startDate,
@@ -21,10 +23,29 @@ class PurposeResponseModel {
   ) {
     return PurposeResponseModel(
       statusCode: AsyncData(statusCode),
+      id: json['id'],
       title: json['title'],
       content: json['content'],
-      startDate: json['startDate'],
-      endDate: json['endDate'],
+      startDate: json['start_date'],
+      endDate: json['end_date'],
+    );
+  }
+
+  PurposeResponseModel copyWith({
+    AsyncValue<int>? statusCode,
+    int? id,
+    String? title,
+    String? content,
+    String? startDate,
+    String? endDate,
+  }) {
+    return PurposeResponseModel(
+      statusCode: statusCode ?? this.statusCode,
+      id: id ?? this.id,
+      title: title ?? this.title,
+      content: content ?? this.content,
+      startDate: startDate ?? this.startDate,
+      endDate: endDate ?? this.endDate,
     );
   }
 }
