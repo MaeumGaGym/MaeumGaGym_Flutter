e05db8b0db3d50975159ab610c3a731a854862c1
유지우 <treeyu12345@naver.com>
2024-05-30 12:28:58 +0900

    FEAT :: 목표 조회 API 연동

--- lib/self_care/presentation/view/purpose/self_care_purpose_detail_screen.dart
+++ lib/self_care/presentation/view/purpose/self_care_purpose_detail_screen.dart
@@ -1,48 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
 import 'package:maeum_ga_gym_flutter/core/component/text/pretendard/ptd_text_widget.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/provider/purpose/self_care_purpose_detail_purposes_provider.dart';
+import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/purpose/widget/self_care_purpose_detail_app_bar.dart';
 import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/purpose/widget/self_care_purpose_time_table_widget.dart';
-import 'package:maeum_ga_gym_flutter/self_care/presentation/widget/self_care_default_app_bar.dart';
 
-class SelfCarePurposeDetailScreen extends StatelessWidget {
-  const SelfCarePurposeDetailScreen({super.key});
+class SelfCarePurposeDetailScreen extends ConsumerStatefulWidget {
+  final int purposeId;
+
+  const SelfCarePurposeDetailScreen({
+    super.key,
+    required this.purposeId,
+  });
+
+  @override
+  ConsumerState<SelfCarePurposeDetailScreen> createState() =>
+      _SelfCarePurposeDetailScreenState();
+}
+
+class _SelfCarePurposeDetailScreenState
+    extends ConsumerState<SelfCarePurposeDetailScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      Duration.zero,
+      () => ref
+          .read(selfCarePurposeDetailPurposesProvider.notifier)
+          .getDetailPurpose(purposeId: widget.purposeId),
+    );
+  }
+
+  String formatDate(String date) {
+    DateTime parsingDate = DateTime.parse(date);
+    String formattedDate = DateFormat('yyyy년 MM월 dd일 (E)', 'ko').format(parsingDate);
+    return formattedDate;
+  }
 
   @override
   Widget build(BuildContext context) {
+    final purposesDetailState =
+        ref.watch(selfCarePurposeDetailPurposesProvider);
     return Scaffold(
       backgroundColor: MaeumgagymColor.white,
-      appBar: const SelfCareDefaultAppBar(
-        iconPath: "assets/image/core_icon/left_arrow_icon.svg",
-        actionIconPath: "assets/image/core_icon/dots_vertical_icon.svg",
-      ),
+      appBar: SelfCarePurposeDetailAppBar(purposeId: widget.purposeId),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              PtdTextWidget.titleLarge(
-                "공부하기",
-                MaeumgagymColor.black,
-              ),
-              const SizedBox(height: 12),
-              const SelfCarePurposeTimeTableWidget(
-                title: "시작",
-                date: "12월 25일 (화) 오전 8:00",
-              ),
-              const SizedBox(height: 12),
+          child: purposesDetailState.statusCode.when(
+            data: (data) {
+              if (data == 200) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    PtdTextWidget.titleLarge(
+                      purposesDetailState.title.toString(),
+                      MaeumgagymColor.black,
+                    ),
+                    const SizedBox(height: 12),
+                    SelfCarePurposeTimeTableWidget(
+                      title: "시작",
+                      date: formatDate(purposesDetailState.startDate.toString()),
+                    ),
+                    const SizedBox(height: 12),
 
-              /// 패딩은 보기가 안좋아서 SizedBox 쓸게요
-              const SelfCarePurposeTimeTableWidget(
-                title: "마감",
-                date: "2024년 01월 01알 (화) 오전 8:00",
-              ),
-              const SizedBox(height: 32),
-              PtdTextWidget.bodyMedium(
-                "새해가 다가오기 전까지 열심히 공부하자.\n한 해의 끝도 공부와 함께.",
-                MaeumgagymColor.black,
-              ),
-            ],
+                    /// 패딩은 보기가 안좋아서 SizedBox 쓸게요
+                    SelfCarePurposeTimeTableWidget(
+                      title: "마감",
+                      date: formatDate(purposesDetailState.endDate.toString()),
+                    ),
+                    const SizedBox(height: 32),
+                    PtdTextWidget.bodyMedium(
+                      purposesDetailState.content.toString(),
+                      MaeumgagymColor.black,
+                    ),
+                  ],
+                );
+              } else {
+                return Text(
+                  "${purposesDetailState.statusCode}",
+                );
+              }
+            },
+            error: (error, stack) {
+              return const Text("에러");
+            },
+            loading: () {
+              return Center(
+                child: CircularProgressIndicator(
+                  color: MaeumgagymColor.blue500,
+                ),
+              );
+            },
           ),
         ),
       ),
