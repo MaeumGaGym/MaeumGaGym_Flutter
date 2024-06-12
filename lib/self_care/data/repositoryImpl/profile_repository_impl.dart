11c8456519d76a9d7565653510d75f796dde7d52
유지우 <treeyu12345@naver.com>
2024-06-12 21:47:19 +0900

    FEAT :: ProfileRepository에 editUserProfile 메서드 추가

--- lib/self_care/data/repositoryImpl/profile_repository_impl.dart
+++ lib/self_care/data/repositoryImpl/profile_repository_impl.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maeum_ga_gym_flutter/self_care/data/date_source/remote/profile_remote_data_source.dart';
 import 'package:maeum_ga_gym_flutter/self_care/domain/model/profile/profile_response_model.dart';
 import 'package:maeum_ga_gym_flutter/self_care/domain/repository/profile_repository.dart';
@@ -13,6 +14,23 @@ class ProfileRepositoryImpl implements ProfileRepository {
     return await _remoteDataSource.getUserProfile(
       accessToken: accessToken,
       nickname: nickname,
+    );
+  }
+
+  @override
+  Future<AsyncValue<int?>> editUserProfile({
+    required String accessToken,
+    required String nickname,
+    required double weight,
+    required double height,
+    required String gender,
+  }) async {
+    return await _remoteDataSource.editUserProfile(
+      accessToken: accessToken,
+      nickname: nickname,
+      weight: weight,
+      height: height,
+      gender: gender,
     );
   }
 }
