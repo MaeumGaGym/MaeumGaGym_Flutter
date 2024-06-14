1b5d25f96404741799b2c390e43aeda1673762fe
ljyo2o9 <yggosxoljy@gmail.com>
2024-06-14 09:09:47 +0900

    FEAT :: poseTabController를 바꾸는 함수를 추가

--- lib/core/component/pose/presentation/provider/pose_tab_provider.dart
+++ lib/core/component/pose/presentation/provider/pose_tab_provider.dart
@@ -1,3 +1,47 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final poseTabController = StateProvider<int>((ref) => 0);
+// final poseTabController = StateProvider<int>((ref) => 0);
+
+final poseTabController =
+StateNotifierProvider<PoseTabStateNotifier, int>(
+        (ref) {
+      return PoseTabStateNotifier();
+    });
+
+class PoseTabStateNotifier extends StateNotifier<int> {
+  PoseTabStateNotifier() : super(0);
+
+  void setTabStateToInt(int index){
+    state = index;
+  }
+
+  void setTabStateToName(String exerciseName){
+    if(exerciseName == "가슴"){
+      state = 1;
+    }
+    else if(exerciseName == "등"){
+      state = 2;
+    }
+    else if(exerciseName == "어깨"){
+      state = 3;
+    }
+    else if(exerciseName == "팔"){
+      state = 4;
+    }
+    else if(exerciseName == "복근"){
+      state = 5;
+    }
+    else if(exerciseName == "앞 허벅지"){
+      state = 6;
+    }
+    else if(exerciseName == "뒷 허벅지"){
+      state = 7;
+    }
+    else if(exerciseName == "종아리"){
+      state = 8;
+    }
+    else {
+      state = 0;
+    }
+  }
+}
