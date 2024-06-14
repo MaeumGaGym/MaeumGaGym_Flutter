e17269dc799ecc0027c1e94e7dc42f056764eb17
유지우 <treeyu12345@naver.com>
2024-06-14 18:49:37 +0900

    RENAME :: 클래스 네이밍 변경

--- lib/self_care/presentation/widget/camera/self_care_camera_widget.dart
+++ lib/self_care/presentation/widget/camera/self_care_camera_widget.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:maeum_ga_gym_flutter/config/maeumgagym_color.dart';
 
-class SelfCareCameraContainerWidget extends StatelessWidget {
+class SelfCareCameraWidget extends StatelessWidget {
   final String iconPath;
 
-  const SelfCareCameraContainerWidget({
+  const SelfCareCameraWidget({
     super.key,
     required this.iconPath,
   });
