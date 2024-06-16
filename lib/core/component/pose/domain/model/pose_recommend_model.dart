2fedeb79da69dd9c1954a4aa86afa83143fefbe8
ljyo2o9 <yggosxoljy@gmail.com>
2024-06-16 15:29:58 +0900

    ENCHANT :: [#489] PoseRecommendModel과 PoseDataModel에 겹치는 부분이 있어 그 부분 수정

--- lib/core/component/pose/domain/model/pose_recommend_model.dart
+++ lib/core/component/pose/domain/model/pose_recommend_model.dart
@@ -1,7 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maeum_ga_gym_flutter/core/component/pose/domain/model/pose_data_model.dart';
 
 class PoseRecommendModel {
-  Map<String, List<PoseData>>? poses;
+  Map<String, List<PoseDataModel>>? poses;
   AsyncValue<int> statusCode;
 
   PoseRecommendModel({
@@ -11,45 +12,16 @@ class PoseRecommendModel {
 
   factory PoseRecommendModel.fromJson(
       Map<String, dynamic> json, int? statusCode) {
-    final Map<String, List<PoseData>> posesMap = {};
+    final Map<String, List<PoseDataModel>> posesMap = {};
     json['poses'].forEach((key, value) {
       posesMap[key] = value['responses']
-          .map<PoseData>((item) => PoseData.fromJson(item))
+          .map<PoseDataModel>((item) => PoseDataModel.fromJson(item))
           .toList();
     });
 
     return PoseRecommendModel(
       poses: posesMap,
       statusCode: AsyncData(statusCode!),
-    );
-  }
-}
-
-class PoseData {
-  int id;
-  bool needMachine;
-  String name;
-  List<String> simplePart;
-  List<String> exactPart;
-  String thumbnail;
-
-  PoseData({
-    required this.id,
-    required this.needMachine,
-    required this.name,
-    required this.simplePart,
-    required this.exactPart,
-    required this.thumbnail,
-  });
-
-  factory PoseData.fromJson(Map<String, dynamic> json) {
-    return PoseData(
-      id: json['id'],
-      needMachine: json['need_machine'],
-      name: json['name'],
-      simplePart: List<String>.from(json['simple_part']),
-      exactPart: List<String>.from(json['exact_part']),
-      thumbnail: json['thumbnail'],
     );
   }
 }
