6976ebd527df0b12d3f38dcb01b41bd61df380e3
ljyo2o9 <yggosxoljy@gmail.com>
2024-06-20 22:34:31 +0900

    FEAT :: [#527] page_manager_controller 수정

--- lib/page_manager/presentation/provider/page_manager_controller.dart
+++ lib/page_manager/presentation/provider/page_manager_controller.dart
@@ -1,21 +1,3 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final pageControllerProvider =
-    StateNotifierProvider<PageControllerNotifier, PageController>((ref) {
-  return PageControllerNotifier();
-});
-
-class PageControllerNotifier extends StateNotifier<PageController> {
-  PageControllerNotifier() : super(PageController(index: 0));
-
-  void setPage(int value) {
-    state = PageController(index: value);
-    print(state.index);
-  }
-}
-
-class PageController {
-  final int index;
-
-  PageController({required this.index});
-}
+final pageControllerProvider = StateProvider<int>((ref) => 0);
