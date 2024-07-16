d5dc2c88368415f74b323626ccff277c22525b28
ljyo2o9 <yggosxoljy@gmail.com>
2024-07-16 19:09:53 +0900

    DOC :: 사용중이 아닐떄 보관하는 내용이 없어 추가

--- lib/self_care/presentation/widget/my_routine/self_care_my_routine_detail_title_widget.dart
+++ lib/self_care/presentation/widget/my_routine/self_care_my_routine_detail_title_widget.dart
@@ -32,13 +32,26 @@ class SelfCareMyRoutineDetailTitleWidget extends ConsumerWidget {
                 MaeumgagymColor.black,
               ),
               const SizedBox(height: 12),
-              if (!item.routineStatus.isArchived)
-                PtdTextWidget.bodyMedium(
-                  item.dayOfWeeks.isEmpty
-                      ? "사용중"
-                      : "사용중 | ${item.dayOfWeeks.map((str) => str[0]).join(", ")}",
-                  MaeumgagymColor.blue500,
-                )
+              Builder(
+                builder: (context) {
+                  if (!item.routineStatus.isArchived){
+                    return PtdTextWidget.bodyMedium(
+                      item.dayOfWeeks.isEmpty
+                          ? "사용중"
+                          : "사용중 | ${item.dayOfWeeks.map((str) => str[0]).join(", ")}",
+                      MaeumgagymColor.blue500,
+                    );
+                  } else {
+                    return PtdTextWidget.bodyMedium(
+                      item.dayOfWeeks.isEmpty
+                          ? "보관중"
+                          : "보관중 | ${item.dayOfWeeks.map((str) => str[0]).join(", ")}",
+                      MaeumgagymColor.gray400,
+                    );
+                  }
+                },
+              )
+
             ],
           ),
         ),
