a2ae22f55c45a7bbc2a7220566f50cb7e0268462
ljyo2o9 <yggosxoljy@gmail.com>
2024-07-16 19:10:44 +0900

    FEAT :: changeRoutineState 추가

--- lib/core/component/routine/presentation/provider/routine_my_routine_my_routine_provider.dart
+++ lib/core/component/routine/presentation/provider/routine_my_routine_my_routine_provider.dart
@@ -59,4 +59,8 @@ class RoutineMyRoutinesStateNotifier
       throw Exception(err.toString());
     }
   }
+
+  Future<void> changeRoutineState({required int index, required bool? isArchived, required bool? isShared}) async {
+    state.routineList[index].routineStatus = state.routineList[index].routineStatus.copyWith(isArchived: isArchived, isShared: isShared);
+  }
 }
